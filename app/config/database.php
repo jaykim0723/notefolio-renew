d4740ac7a079db9fca2c433b37b7750765307f93
SnooeyNET <sople1@snooey.net>
2013-11-20 17:15:23 +0900

    error database.php

--- app/config/database.php
+++ app/config/database.php
@@ -48,11 +48,11 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'zidell';
-$db['default']['password'] = 'ps';
+$db['default']['hostname'] = 'server.notefolio.net';
+$db['default']['username'] = 'notefolio-renew';
+$db['default']['password'] = 'ay5rvX29doo25y22';
 $db['default']['database'] = 'notefolio-renew';
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
