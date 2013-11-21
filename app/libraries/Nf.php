51fc9561c53deeaf8693a2b8ba0a341ccea4367f
zidell <zidell@gmail.com>
2013-11-21 15:00:08 +0900

    Revert "aoeuuuu"
    This reverts commit 52d062f3cd3197a0ba90fc777646eccf8b949b94.

---
+++ app/libraries/Nf.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Nf
+{
+	protected	$ci;
+	public function __construct()
+	{
+        $this->ci =& get_instance();
+	}
+
+
+
+
+
+	
+
+}
