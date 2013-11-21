d4d65cc001f0f431fe68b24e868a05d2d9638f74
SnooeyNET <sople1@snooey.net>
2013-11-21 11:36:21 +0900

    기본코드 이전

---
+++ app/libraries/fbsdk.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * @brief Facebook SDK Connect Library
+ *
+ * @author Yoon, Seongsu(soplel@snooey.net)
+ */
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+require_once (APPPATH.'libraries/fb-sdk/facebook.php');
+
+class Fbsdk extends Facebook
+{
+    var $last_response = "";
+    
+    function __construct($config=null) {
+        $this->ci =& get_instance();
+        
+        if($config==null) {
+            $config = array(
+                  'appId'  => $this->ci->config->item('facebook_app_id'),
+                  'secret' => $this->ci->config->item('facebook_app_secret'),
+                  'sharedSession' => FALSE,
+                  'trustForwarded' => true,
+            );
+        }
+        parent::__construct($config);
+    }
+    
+    /*
+     * set data for facebook
+     * 
+     * @param int $user_id, array $data
+     * 
+     * @return bool
+     */
+    function set_data($user_id=0, $data=array())
+    {
+        if($user_id==0) $user_id = $this->ci->tank_auth->get_user_id();
+        $this->ci->load->model('api/auth_model');
+        if(count($data)>0&&isset($data['action'])){
+            switch($data['action']){
+                case "unlink":
+                    return $this->ci->auth_model->delete_user_fb_info($user_id);
+                    break;
+            }
+        }
+        
+        return FALSE;
+    }
+    
+    /*
+     * post data to facebook
+     * 
+     * @param int $user_id, array $data
+     * 
+     * @return int
+     */
+    function post_data($user_id=0, $data=array())
+    {
+        if($user_id==0) $user_id = $this->ci->tank_auth->get_user_id();
+        $this->ci->load->model('api/auth_model');
+        $fb_info = $this->ci->auth_model->get_user_fb_info($user_id);
+        
+        if ($fb_info[$data['type']]=='Y') {
+            if(count($data)>0&&isset($data['type'])){
+                $post['access_token'] = $fb_info['access_token'];
+                switch($data['type']){
+                    case "post_work":
+                        $target = ":upload";
+                        $post['work'] = $this->ci->config->item('base_url')."gallery/".$data['work_id'];
+                        break;
+                    case "post_comment":
+                        $target = ":comment";
+                        $post['work'] = $this->ci->config->item('base_url')."gallery/".$data['work_id'];
+                        break;
+                    case "post_note":
+                        $target = ":note";
+                        $post['work'] = $this->ci->config->item('base_url')."gallery/".$data['work_id'];
+                        break;
+                    case "post_test":
+                        $target = ":test";
+                        $post['work'] = $this->ci->config->item('base_url')."gallery/".$data['work_id'];
+                        break;
+                }
+                try {
+                    $this->last_response = $this->api('/me/notefolio'.$target, 'POST', $post);
+                } catch (FacebookApiException $e) {
+                    error_log($e);
+                    error_log($this->last_response);
+                    return FALSE;
+                }
+            }
+        } else {
+            $this->last_response=json_encode(array("error"=>array("code"=>0, "message"=>"Cannot Send because something wrong.", "user_id"=>$user_id, "data"=>$data, "fb_info"=>$fb_info)));
+            return FALSE;
+        }
+        
+        return TRUE;
+    }
+}
+
+?>
