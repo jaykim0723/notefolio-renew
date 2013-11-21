a3181ed3279a52f57f2ab65184c3fd9d405e2b0e
SnooeyNET <sople1@snooey.net>
2013-11-21 18:35:25 +0900

    uaaaa....

--- app/views/gallery/listing_view.php
+++ app/views/gallery/listing_view.php
@@ -65,6 +65,7 @@
 			container: '.thumbnail_list',
 			items: '.thumbbox',
   			more: '.more-link',
+    		offset: 'bottom-in-view',
 			onAfterPageLoad : function(){
 				console.log($.now());
 			}
