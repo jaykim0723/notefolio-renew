4e519e437d49de1e53152eb6d8d9ea5c941df9e9
SnooeyNET <sople1@snooey.net>
2013-11-21 16:26:07 +0900

    get out, infinite loop

--- app/views/gallery/listing_view.php
+++ app/views/gallery/listing_view.php
@@ -18,20 +18,48 @@
 		<div class="row">
 			<div class="col-md-12">
 <?php endif ?>
-				<div class="thumbnail_list infinite-container">
+				<div class="thumbnail_list">
 					<!-- list -->
 					<?php foreach ($rows as $key => $row): ?>
 					<?php $this->load->view('gallery/thumbnail_inc_view', $row) ?>
 					<?php endforeach ?>
 				</div>
-				<a href="/gallery/listing/2" class="infinite-more-link">more</a>
+				<a href="/gallery/listing/2" class="more-link">more</a>
 <?php if (!$this->input->is_ajax_request()): ?>
 			</div>
 		</div>
 	</div>
 </section>
 <script>
+	var nextPage = true;
+	function callPage(){
+		var url = window.location.href;
+		var match = new RegExp("/listing/([0-9]+)").exec(url);
+		if(match==null){
+			url = url.replace("/listing", '/listing/2');
+		}
+		else {
+			url = url.replace(match[0], '/listing/'+(Number(match[1])+1));
+		}
+
+		$.get(url).done(function(data){
+			$('.thumbbox', data).each(function(){
+				$(this).appendTo('.thumbnail_list');
+			});
+			if($('.more-link', data).length>0){
+				$('.more-link').replaceWith($('.more-link', data));
+			}
+			else {
+				nextPage = false;
+				$('.more-link').remove();
+			}
+		});
+	}
 	$(function() {
+		$('.thumbnail_list').waypoint(function(){
+			if(nextPage) callPage()
+		});
+		/*
 		$('.thumbnail_list').waypoint('infinite', {
   			container: '.thumbnail_list',
 			items: '.thumbbox',
@@ -39,6 +67,7 @@
 				console.log($.now());
 			}
 		});
+		*/
 	});
 </script>
 <?php endif; ?>
