886bcfcfc02fc1f773573cb7bc3711c1c48140b3
SnooeyNET <sople1@snooey.net>
2013-11-21 20:26:25 +0900

    infinite

--- www/js/libs/waypoints-infinite.js
+++ www/js/libs/waypoints-infinite.js
@@ -50,7 +50,7 @@ https://github.com/imakewebthings/jquery-waypoints/blob/master/licenses.txt
             $newMore = $data.find(options.more);
             $container.append($data.find(options.items));
             $container.removeClass(options.loadingClass);
-            console.log($newMore.length);
+            console.log(options.items);
             if ($newMore.length) {
               $more.replaceWith($newMore);
               $this.waypoint('enable');
