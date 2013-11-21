fd10194422fa0179961d626e9a6cee89f318e5fb
SnooeyNET <sople1@snooey.net>
2013-11-21 20:23:04 +0900

    123456789009

--- app/views/layout/footer_inc_view.php
+++ app/views/layout/footer_inc_view.php
@@ -1,7 +1,7 @@
 <script src="/js/libs/bootstrap.min.js"></script>
 <script src="/js/libs/bootstrap.min.js"></script>
 <script src="/js/libs/waypoints.min.js"></script>
-<script src="/js/libs/waypoints-infinite.min.js"></script>
+<script src="/js/libs/waypoints-infinite.js"></script>
 <script src="/js/libs/waypoints-sticky.min.js"></script>
 <script src="/js/libs/jquery.history.js"></script>
 
