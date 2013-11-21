25be3a599bdb40ffa2a279811ba94ee15480a6d8
zidell <zidell@gmail.com>
2013-11-21 15:00:35 +0900

    aa

--- app/controllers/gallery.php
+++ app/controllers/gallery.php
@@ -1,4 +1,4 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+nt<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Gallery extends CI_Controller {
 
