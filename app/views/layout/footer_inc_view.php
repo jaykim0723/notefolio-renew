9fbf146c6cb45a2f8c145b839c3571639ac91f4a
SnooeyNET <sople1@snooey.net>
2013-11-22 11:38:31 +0900

    waypoint sticky fix

--- app/views/layout/footer_inc_view.php
+++ app/views/layout/footer_inc_view.php
@@ -2,7 +2,7 @@
 <script src="/js/libs/bootstrap.min.js"></script>
 <script src="/js/libs/waypoints.min.js"></script>
 <script src="/js/libs/waypoints-infinite.js"></script>
-<script src="/js/libs/waypoints-sticky.min.js"></script>
+<script src="/js/libs/waypoints-sticky.js"></script>
 <script src="/js/libs/jquery.history.js"></script>
 
 </body>
