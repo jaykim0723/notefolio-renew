5952e53034ba3416695b00d0780d9e3e8b9c3f95
SnooeyNET <sople1@snooey.net>
2013-11-25 15:02:56 +0900

    1

--- app/models/work_model.php
+++ app/models/work_model.php
@@ -48,7 +48,7 @@ class work_model extends CI_Model {
     		->select('works.id as work_id, title, realname as user, regdate, keywords, tags, user_id, folder, contents, moddate, hit_cnt, note_cnt, comment_cnt, collect_cnt, ccl, discoverbility')
     		->from('works')
     		->join('users', 'users.id = works.user_id', 'left')
-    		->where('works.id' => $work_id)
+    		->where('works.id', $work_id)
     		->limit(1, 0); //set table
 		var_export($data->get_compiled_select());
 
