9255edeadf4261255904b4b3d81e9eb9542ab115
SnooeyNET <sople1@snooey.net>
2013-11-27 18:23:11 +0900

    1

--- app/libraries/Nf.php
+++ app/libraries/Nf.php
@@ -6,18 +6,21 @@ class Nf
 	public function __construct()
 	{
         $this->ci =& get_instance();
+
+        if ($this->ci->tank_auth->is_logged_in()) {
+            if(!defined('USER_ID'))
+                define('USER_ID', $this->ci->tank_auth->get_user_id());
+        } else {
+            if(!defined('USER_ID'))
+                define('USER_ID', 0);
+        }
 	}
 
 
     function _member_check($member_only=array(), $go_to='')
     {
         $this->ci =& get_instance();
-        if ($this->ci->tank_auth->is_logged_in()) {
-        	if(!defined('USER_ID'))
-	            define('USER_ID', $this->ci->tank_auth->get_user_id());
-        } else {
-        	if(!defined('USER_ID'))
-	            define('USER_ID', 0);
+        if (!$this->ci->tank_auth->is_logged_in()) {
             if(in_array($this->ci->router->fetch_method(), $member_only)) {
             	if($this->ci->input->is_ajax_request()){
             		exit('login');
