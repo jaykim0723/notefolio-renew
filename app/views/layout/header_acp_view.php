18450fa4ad5e81508b6409075722c7f4a03753fe
SnooeyNET <sople1@snooey.net>
2013-11-27 16:33:08 +0900

    1

--- app/views/layout/header_acp_view.php
+++ app/views/layout/header_acp_view.php
@@ -20,12 +20,19 @@
 <body>
 <div class="navbar navbar-fixed-top">
   <div class="navbar-inner">
-    <div class="container"> <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span
-                    class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span> </a><a class="brand" href="/acp/">Admin</a>
+    <div class="container">
+    	<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+    		<span class="icon-bar"></span>
+    		<span class="icon-bar"></span>
+    		<span class="icon-bar"></span> 
+    	</a>
+    	<a class="brand" href="/acp/">Admin</a>
       <div class="nav-collapse">
         <ul class="nav pull-right">
-          <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i
-                            class="icon-user"></i> notefolio <b class="caret"></b></a>
+          <li class="dropdown">
+          	<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+          		<i class="icon-user"></i> notefolio <b class="caret"></b>
+          	</a>
             <ul class="dropdown-menu">
               <li><a href="javascript:;">Profile</a></li>
               <li><a href="javascript:;">Logout</a></li>
@@ -47,19 +54,40 @@
   <div class="subnavbar-inner">
     <div class="container">
       <ul class="mainnav">
-        <li class="active"><a href="index.html"><i class="icon-dashboard"></i><span>Dashboard</span> </a> </li>
-        <li><a href="reports.html"><i class="icon-list-alt"></i><span>Reports</span> </a> </li>
-        <li><a href="guidely.html"><i class="icon-facetime-video"></i><span>App Tour</span> </a></li>
-        <li><a href="charts.html"><i class="icon-bar-chart"></i><span>Charts</span> </a> </li>
-        <li><a href="shortcodes.html"><i class="icon-code"></i><span>Shortcodes</span> </a> </li>
-        <li class="dropdown"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-long-arrow-down"></i><span>Drops</span> <b class="caret"></b></a>
+        <li id="menu-dashboard" class="active">
+        	<a href="/acp/dashboard">
+        		<i class="icon-dashboard"></i>
+        		<span>대시보드</span>
+        	</a>
+        </li>
+        <li id="menu-site" class="dropdown">
+        	<a href="/acp/site" class="dropdown-toggle" data-toggle="dropdown">
+        		<i class="icon-list-alt"></i>
+        		<span>사이트</span>
+        		<b class="caret"></b>
+        	</a>
           <ul class="dropdown-menu">
             <li><a href="icons.html">Icons</a></li>
-            <li><a href="faq.html">FAQ</a></li>
-            <li><a href="pricing.html">Pricing Plans</a></li>
-            <li><a href="login.html">Login</a></li>
-            <li><a href="signup.html">Signup</a></li>
-            <li><a href="error.html">404</a></li>
+          </ul>
+        </li>
+        <li id="menu-user" class="dropdown">
+        	<a href="/acp/user" class="dropdown-toggle" data-toggle="dropdown">
+        		<i class="icon-list-alt"></i>
+        		<span>사용자</span>
+        		<b class="caret"></b>
+        	</a>
+          <ul class="dropdown-menu">
+            <li><a href="icons.html">Icons</a></li>
+          </ul>
+        </li>
+        <li id="menu-work" class="dropdown">
+        	<a href="/acp/work" class="dropdown-toggle" data-toggle="dropdown">
+        		<i class="icon-list-alt"></i>
+        		<span>작품</span>
+        		<b class="caret"></b>
+        	</a>
+          <ul class="dropdown-menu">
+            <li><a href="icons.html">Icons</a></li>
           </ul>
         </li>
       </ul>
