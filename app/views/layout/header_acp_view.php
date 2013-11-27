d1a7826cb01c01e1f047ffa81b52d8dab2e38c5f
SnooeyNET <sople1@snooey.net>
2013-11-27 15:55:05 +0900

    1

--- app/views/layout/header_acp_view.php
+++ app/views/layout/header_acp_view.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 <title>Notefolio Admin</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-capable" content="yes"><link href="/css/normalize.css" rel="stylesheet"/>
 <link href="/css/bootstrap.min.css" rel="stylesheet">
 <link href="/css/bootstrap-responsive.min.css" rel="stylesheet">
 <link href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600"
