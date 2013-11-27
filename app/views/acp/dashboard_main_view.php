3b8565284efd837f9d3e5417d48779d969d6863d
SnooeyNET <sople1@snooey.net>
2013-11-27 15:27:15 +0900

    1

--- app/views/acp/dashboard_main_view.php
+++ app/views/acp/dashboard_main_view.php
@@ -51,7 +51,7 @@
             <!-- /widget-header -->
             <div class="widget-content">
               <ul class="messages_layout">
-                <li class="from_user left"> <a href="#" class="avatar"><img src="img/message_avatar1.png"/></a>
+                <li class="from_user left"> <a href="#" class="avatar"><img src="/img/message_avatar1.png"/></a>
                   <div class="message_wrap"> <span class="arrow"></span>
                     <div class="info"> <a class="name">John Smith</a> <span class="time">1 hour ago</span>
                       <div class="options_arrow">
@@ -67,7 +67,7 @@
                     <div class="text"> As an interesting side note, as a head without a body, I envy the dead. There's one way and only one way to determine if an animal is intelligent. Dissect its brain! Man, I'm sore all over. I feel like I just went ten rounds with mighty Thor. </div>
                   </div>
                 </li>
-                <li class="by_myself right"> <a href="#" class="avatar"><img src="img/message_avatar2.png"/></a>
+                <li class="by_myself right"> <a href="#" class="avatar"><img src="/img/message_avatar2.png"/></a>
                   <div class="message_wrap"> <span class="arrow"></span>
                     <div class="info"> <a class="name">Bender (myself) </a> <span class="time">4 hours ago</span>
                       <div class="options_arrow">
@@ -83,7 +83,7 @@
                     <div class="text"> All I want is to be a monkey of moderate intelligence who wears a suit… that's why I'm transferring to business school! I had more, but you go ahead. Man, I'm sore all over. I feel like I just went ten rounds with mighty Thor. File not found. </div>
                   </div>
                 </li>
-                <li class="from_user left"> <a href="#" class="avatar"><img src="img/message_avatar1.png"/></a>
+                <li class="from_user left"> <a href="#" class="avatar"><img src="/img/message_avatar1.png"/></a>
                   <div class="message_wrap"> <span class="arrow"></span>
                     <div class="info"> <a class="name">Celeste Holm </a> <span class="time">1 Day ago</span>
                       <div class="options_arrow">
@@ -99,7 +99,7 @@
                     <div class="text"> And I'd do it again! And perhaps a third time! But that would be it. Are you crazy? I can't swallow that. And I'm his friend Jesus. No, I'm Santa Claus! And from now on you're all named Bender Jr. </div>
                   </div>
                 </li>
-                <li class="from_user left"> <a href="#" class="avatar"><img src="img/message_avatar2.png"/></a>
+                <li class="from_user left"> <a href="#" class="avatar"><img src="/img/message_avatar2.png"/></a>
                   <div class="message_wrap"> <span class="arrow"></span>
                     <div class="info"> <a class="name">Mark Jobs </a> <span class="time">2 Days ago</span>
                       <div class="options_arrow">
