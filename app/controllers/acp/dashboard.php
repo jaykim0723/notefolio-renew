879dc94eb2a5137a97ee3bfe3b21db959836632d
SnooeyNET <sople1@snooey.net>
2013-11-27 14:46:34 +0900

    1234

---
+++ app/controllers/acp/dashboard.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+		$this->nf->_admin_check();
+    }
+	
+    /**
+     * lang:ko;실제 dashboard는 메인 페이지만 필요합니다.
+	 *
+	 */
+	public function index()
+	{
+		$this->layout->set_view('acp/dashboard')->render();
+	}
+}
+
+/* End of file dashboard.php */
+/* Location: ./application/controllers/acp/dashboard.php */
