6cedfc9aa9b2e03677b2051d3e2f5b963f57681b
SnooeyNET <sople1@snooey.net>
2013-11-27 19:18:12 +0900

    1

--- app/controllers/auth.php
+++ app/controllers/auth.php
@@ -187,8 +187,18 @@ class Auth extends CI_Controller
                         '',
                         $this->data['login_by_username'],
                         $this->data['login_by_email'])) {                             // success
-                    $this->nf->admin_elevate();
-                    redirect(!empty($go_to)?$go_to:'/acp');
+                    if($this->nf->admin_elevate());
+                    {
+                        // go_to에 따라 가야할 곳을 지정함.
+                        $is_ajax?
+                            die(json_encode(array('status'=>'success', 'type'=>'elevated')))
+                            :redirect(!empty($go_to)?$go_to:'/acp');
+                    }
+                    else {
+                        // go_to에 따라 가야할 곳을 지정함.
+                        $is_ajax?
+                            die(json_encode(array('status'=>'error', 'type'=>'not_elevated')));
+                    }
 
                 } else {
                     $errors = $this->tank_auth->get_error_message();
