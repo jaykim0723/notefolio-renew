8c8493a237d941a8f62fd40ed34ef57857f59b40
SnooeyNET <sople1@snooey.net>
2013-11-29 16:44:07 +0900

    1

--- app/models/user_model.php
+++ app/models/user_model.php
@@ -68,7 +68,7 @@ class user_model extends CI_Model {
                 $addr .= ', '.$profile_fields.'.'.$field;
             }
         }
-        
+
     	$this->db
             ->select('users.*'.$addr)
     		->from('users')
@@ -129,6 +129,18 @@ class user_model extends CI_Model {
                 $params->{$key} = $value;
         }
 
+        $addr = '';
+        if($params->get_profile){
+            $profile_table = "user_profiles";
+            $profile_fields = array('user_id', 'website', 'facebook_id',
+                                    'twitter_id', 'gender', 'birth',
+                                    'description', 'mailing',
+                                    'following_cnt', 'follower_cnt');
+            foreach($profile_fields as $field){
+                $addr .= ', '.$profile_fields.'.'.$field;
+            }
+        }
+
     	$this->db
             ->select('users.*')
     		->from('users')
@@ -136,13 +148,7 @@ class user_model extends CI_Model {
     		->limit(1); //set
 
         if($params->get_profile){
-            $this->db->select('user_profiles.user_id,    user_profiles.website,
-                               user_profiles.profile_id, user_profiles.facebook_id,
-                               user_profiles.twitter_id, user_profiles.gender, 
-                               user_profiles.birth,      user_profiles.description,
-                               user_profiles.mailing,    user_profiles.following_cnt,
-                               user_profiles.follower_cnt  ')
-                     ->join('user_profiles', 'users.id=user_profiles.user_id', 'left');
+            $this->db->join('user_profiles', 'users.id=user_profiles.user_id', 'left');
         }
 
         $user = $this->db->get()->row();
