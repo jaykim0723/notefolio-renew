17c97b7ffa572acbd11cf03044fd1c6b8de004ac
SnooeyNET <sople1@snooey.net>
2013-12-03 17:03:55 +0900

    1

--- www/js/work.js
+++ www/js/work.js
@@ -42,8 +42,8 @@ var workUtil = {
 			}
 			$(target).droppable({
 		      drop: function( event, ui ) {
-				console.log(ui.droppable.attr("class"));
-				ui.droppable.remove();
+				console.log($(ui.draggable).attr("class"));
+				$(ui.draggable).remove();
 		      }
 		    });
 		},
@@ -62,8 +62,8 @@ var workUtil = {
 				helper: "clone",
 				revert: "valid",
 				drop: function( event, ui ){
-					console.log(ui.draggable.attr("class"));
-					ui.draggable.remove();
+					console.log($(ui.draggable).attr("class"));
+					$(ui.draggable).remove();
 				}
 			});
 		},
