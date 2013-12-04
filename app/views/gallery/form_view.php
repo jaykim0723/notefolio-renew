f816ac016b71635a1a8b5b9448a1739fd2d8ec7e
SnooeyNET <sople1@snooey.net>
2013-12-04 11:51:02 +0900

    1

--- app/views/gallery/form_view.php
+++ app/views/gallery/form_view.php
@@ -107,11 +107,7 @@
 	<li class="block-text"><i class="glyphicon glyphicon-pencil"></i></li>
 	<li class="block-image"><i class="glyphicon glyphicon-picture"></i></li>
 	<li class="block-video"><i class="glyphicon glyphicon-film"></i></li>
-	<li class="trash-bin-box">
-		<i class="glyphicon glyphicon-remove"></i>
-		<div id="trash-bin">
-		</div>
-	</li>
+	<li id="trash-bin"><i class="glyphicon glyphicon-remove"></i></li>
 </ul>
 <script>
 	NFview = <?php
