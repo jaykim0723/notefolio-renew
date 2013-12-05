bc54daf032cc2d5419031c1444479c93a7f161ea
SnooeyNET <sople1@snooey.net>
2013-12-05 16:33:39 +0900

    1

--- www/js/work.js
+++ www/js/work.js
@@ -172,7 +172,7 @@ var workUtil = {
 							maxFilesize: 128, // MB
 							init: function() {
 								return $(this.element)
-									.append($('<div class="uploader-text"></div>').text('이곳을 클릭하거나 파일을 끌어오세요.'));
+									.after($('<div class="uploader-text"></div>').text('이곳을 클릭하거나 파일을 끌어오세요.'));
 							},
 							fallback: function() {
 						        var msg 	 = $('<p>Internet Explorer 9 이하 버전은 기존 업로드 기능을 그대로 이용하고 있습니다.</p>');
