58e95acdcc836604badc02d5cb99dae2ff9c54ed
SnooeyNET <sople1@snooey.net>
2013-12-06 18:53:46 +0900

    1

--- app/controllers/upload.php
+++ app/controllers/upload.php
@@ -316,11 +316,11 @@ class Upload extends CI_Controller
 	}
 	
 	function image($file=null){
-		
+
 
 		if($file=null){
 			$json = array(
-				'status' => 'error',
+				'status' => 'fail',
 				'message' => 'not_file_received'
 				);
 		}
