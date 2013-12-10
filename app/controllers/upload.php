0096497e54763cb105eb5e414e6625ff3fd90535
SnooeyNET <sople1@snooey.net>
2013-12-10 17:11:56 +0900

    1

--- app/controllers/upload.php
+++ app/controllers/upload.php
@@ -199,18 +199,18 @@ class Upload extends CI_Controller
 			if ($image->getImageColorspace() == Imagick::COLORSPACE_CMYK) { 
 			    $profiles = $image->getImageProfiles('*', false); 
 			    // we're only interested if ICC profile(s) exist 
-			    $has_icc_profile = (array_search('icc', $profiles) !== false); 
-				exit(var_export($profile));
+			    $has_icc_profile = (array_search('icc', $profiles) !== false);
 			    // if it doesnt have a CMYK ICC profile, we add one 
 			    if ($has_icc_profile === false) { 
-			       $icc_cmyk = file_get_contents(APPPATH.'tools/colorspace/USWebUncoated.icc'); 
-			       $image->profileImage('icc', $icc_cmyk); 
-			       unset($icc_cmyk); 
+			        $icc_cmyk = file_get_contents(APPPATH.'tools/colorspace/USWebUncoated.icc');  
+			        $image->profileImage('icc', $icc_cmyk); 
+			        unset($icc_cmyk); 
 			    } 
 			    // then we add an RGB profile 
 			    $icc_rgb = file_get_contents(APPPATH.'tools/colorspace/sRGB_v4_ICC_preference.icc'); 
 			    $image->profileImage('icc', $icc_rgb); 
 			    unset($icc_rgb); 
+					exit(var_export($profile));
 			}
 
 			if(in_array($todo, array('crop'))){
