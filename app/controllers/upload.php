66bee65fb25e932dd3c07da82cb58f2ad131703a
SnooeyNET <sople1@snooey.net>
2013-12-10 12:08:17 +0900

    push colorspace file

--- app/controllers/upload.php
+++ app/controllers/upload.php
@@ -194,7 +194,22 @@ class Upload extends CI_Controller
 
 			// assign ImageMagick
 			$image = new Imagick($file['tmp_name']);
-			$image->setImageColorspace(Imagick::COLORSPACE_SRGB);
+			//$image->setImageColorspace(Imagick::COLORSPACE_SRGB);
+			if ($image->getImageColorspace() == Imagick::COLORSPACE_CMYK) { 
+			    $profiles = $image->getImageProfiles('*', false); 
+			    // we're only interested if ICC profile(s) exist 
+			    $has_icc_profile = (array_search('icc', $profiles) !== false); 
+			    // if it doesnt have a CMYK ICC profile, we add one 
+			    if ($has_icc_profile === false) { 
+			       $icc_cmyk = file_get_contents(APPPATH.'tools/colorspace/USWebUncoated.icc'); 
+			       $image->profileImage('icc', $icc_cmyk); 
+			       unset($icc_cmyk); 
+			    } 
+			    // then we add an RGB profile 
+			    $icc_rgb = file_get_contents(APPPATH.'tools/colorspace/sRGB_v4_ICC_preference.icc'); 
+			    $image->profileImage('icc', $icc_rgb); 
+			    unset($icc_rgb); 
+			}
 
 			if(in_array($todo, array('crop'))){
 				// Crop Image. Resize is next block.
