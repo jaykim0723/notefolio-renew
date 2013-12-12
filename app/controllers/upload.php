b1c8fcb3f0848a5368ea2ef9b22f2ff4ae541900
SnooeyNET <sople1@snooey.net>
2013-12-12 17:04:20 +0900

    1

--- app/controllers/upload.php
+++ app/controllers/upload.php
@@ -192,51 +192,57 @@ class Upload extends CI_Controller
 				default:
 				break;
 			}
-
-			// assign ImageMagick
-			$image = new Imagick($file['tmp_name']);
-			//$image->setImageColorspace(Imagick::COLORSPACE_SRGB); // color is inverted
-			if ($image->getImageColorspace() == Imagick::COLORSPACE_CMYK) { 
-			    $profiles = $image->getImageProfiles('*', false); 
-			    // we're only interested if ICC profile(s) exist 
-			    $has_icc_profile = (array_search('icc', $profiles) !== false);
-			    // if it doesnt have a CMYK ICC profile, we add one 
-			    if ($has_icc_profile === false) { 
-			        $icc_cmyk = file_get_contents(APPPATH.'libraries/colorspace/USWebUncoated.icc');  
-			        $image->profileImage('icc', $icc_cmyk); 
-			        unset($icc_cmyk); 
-			    } 
-			    // then we add an RGB profile 
-			    $icc_rgb = file_get_contents(APPPATH.'libraries/colorspace/sRGB_v4_ICC_preference.icc'); 
-			    $image->profileImage('icc', $icc_rgb); 
-			    unset($icc_rgb); 
-			}
-
-	    	$image->resampleImage(72,72,imagick::FILTER_LANCZOS,0);
-
-			if(in_array('crop', $todo)){
-				// Crop Image. Resize is next block.
-				$image->cropImage($width, $height, $x, $y);
-			}
-
-			if(in_array('resize', $todo)){
-		    	if($image->getImageWidth() > $max_width){
-				// Resize image using the lanczos resampling algorithm based on width
-					$image->resizeImage($max_width,$max_height,Imagick::FILTER_LANCZOS,0);
+			if(class_exists('Imagick')){
+				// assign ImageMagick
+				$image = new Imagick($file['tmp_name']);
+				//$image->setImageColorspace(Imagick::COLORSPACE_SRGB); // color is inverted
+				if ($image->getImageColorspace() == Imagick::COLORSPACE_CMYK) { 
+				    $profiles = $image->getImageProfiles('*', false); 
+				    // we're only interested if ICC profile(s) exist 
+				    $has_icc_profile = (array_search('icc', $profiles) !== false);
+				    // if it doesnt have a CMYK ICC profile, we add one 
+				    if ($has_icc_profile === false) { 
+				        $icc_cmyk = file_get_contents(APPPATH.'libraries/colorspace/USWebUncoated.icc');  
+				        $image->profileImage('icc', $icc_cmyk); 
+				        unset($icc_cmyk); 
+				    } 
+				    // then we add an RGB profile 
+				    $icc_rgb = file_get_contents(APPPATH.'libraries/colorspace/sRGB_v4_ICC_preference.icc'); 
+				    $image->profileImage('icc', $icc_rgb); 
+				    unset($icc_rgb); 
 				}
+
+		    	$image->resampleImage(72,72,imagick::FILTER_LANCZOS,0);
+
+				if(in_array('crop', $todo)){
+					// Crop Image. Resize is next block.
+					$image->cropImage($width, $height, $x, $y);
+				}
+
+				if(in_array('resize', $todo)){
+			    	if($image->getImageWidth() > $max_width){
+					// Resize image using the lanczos resampling algorithm based on width
+						$image->resizeImage($max_width,$max_height,Imagick::FILTER_LANCZOS,0);
+					}
+				}
+
+				// Set Image format n quality
+				$image->setImageFormat((isset($opt['ext'])&&$opt['ext']!='')?$opt['ext']:'jpg');
+				//$image->setImageFormat('jpeg');
+	        	$image->setImageCompressionQuality((isset($opt['ext'])&&$opt['ext']!='jpg')?0:90);
+				
+				// Clean & Save
+				$image->stripImage();
+				$image->writeImage($name);
+				$image->destroy();
+
+				return true;
 			}
-
-			// Set Image format n quality
-			$image->setImageFormat((isset($opt['ext'])&&$opt['ext']!='')?$opt['ext']:'jpg');
-			//$image->setImageFormat('jpeg');
-        	$image->setImageCompressionQuality((isset($opt['ext'])&&$opt['ext']!='jpg')?0:90);
-			
-			// Clean & Save
-			$image->stripImage();
-			$image->writeImage($name);
-			$image->destroy();
-
+			else {
+				
+			}
 		}
+
 		return false;
 	}
 
