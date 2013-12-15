ed1ed50d3b5b4b217fb5fb348b5bdb29150f7131
zidell <zidell@gmail.com>
2013-12-15 14:08:01 +0900

    register form

--- app/views/auth/register_form.php
+++ app/views/auth/register_form.php
@@ -3,6 +3,7 @@ if ($use_username) {
 	$username = array(
 		'name'	=> 'username',
 		'id'	=> 'username',
+		'class' => 'form-control',
 		'value' => set_value('username'),
 		'maxlength'	=> $this->config->item('username_max_length', 'tank_auth'),
 		'size'	=> 30,
@@ -11,6 +12,7 @@ if ($use_username) {
 $email = array(
 	'name'	=> 'email',
 	'id'	=> 'email',
+	'class' => 'form-control',
 	'value'	=> set_value('email'),
 	'maxlength'	=> 80,
 	'size'	=> 30,
@@ -18,6 +20,7 @@ $email = array(
 $password = array(
 	'name'	=> 'password',
 	'id'	=> 'password',
+	'class' => 'form-control',
 	'value' => set_value('password'),
 	'maxlength'	=> $this->config->item('password_max_length', 'tank_auth'),
 	'size'	=> 30,
@@ -25,6 +28,7 @@ $password = array(
 $confirm_password = array(
 	'name'	=> 'confirm_password',
 	'id'	=> 'confirm_password',
+	'class' => 'form-control',
 	'value' => set_value('confirm_password'),
 	'maxlength'	=> $this->config->item('password_max_length', 'tank_auth'),
 	'size'	=> 30,
@@ -32,69 +36,96 @@ $confirm_password = array(
 $captcha = array(
 	'name'	=> 'captcha',
 	'id'	=> 'captcha',
+	'class' => 'form-control',
 	'maxlength'	=> 8,
 );
 ?>
-<?php echo form_open($this->uri->uri_string()); ?>
-<table>
+<?php echo form_open($this->uri->uri_string(), array('role'=>'form')); ?>
+	<div class="form-group">
+		<a href="" class="btn btn-info btn-block">Signup with facebook</a>
+	</div>
 	<?php if ($use_username) { ?>
-	<tr>
-		<td><?php echo form_label('Username', $username['id']); ?></td>
-		<td><?php echo form_input($username); ?></td>
-		<td style="color: red;"><?php echo form_error($username['name']); ?><?php echo isset($errors[$username['name']])?$errors[$username['name']]:''; ?></td>
-	</tr>
+	<div class="form-group">
+		<?php echo form_label('Username', $username['id']); ?>
+		<?php echo form_input($username); ?>
+		<?php echo form_error($username['name']); ?><?php echo isset($errors[$username['name']])?$errors[$username['name']]:''; ?>
+	</div>
 	<?php } ?>
-	<tr>
-		<td><?php echo form_label('Email Address', $email['id']); ?></td>
-		<td><?php echo form_input($email); ?></td>
-		<td style="color: red;"><?php echo form_error($email['name']); ?><?php echo isset($errors[$email['name']])?$errors[$email['name']]:''; ?></td>
-	</tr>
-	<tr>
-		<td><?php echo form_label('Password', $password['id']); ?></td>
-		<td><?php echo form_password($password); ?></td>
-		<td style="color: red;"><?php echo form_error($password['name']); ?></td>
-	</tr>
-	<tr>
-		<td><?php echo form_label('Confirm Password', $confirm_password['id']); ?></td>
-		<td><?php echo form_password($confirm_password); ?></td>
-		<td style="color: red;"><?php echo form_error($confirm_password['name']); ?></td>
-	</tr>
+	<div class="form-group">
+		<?php echo form_label('Email Address', $email['id']); ?>
+		<?php echo form_input($email); ?>
+		<?php echo form_error($email['name']); ?><?php echo isset($errors[$email['name']])?$errors[$email['name']]:''; ?>
+	</div>
+	<div class="form-group">
+		<?php echo form_label('Password', $password['id']); ?>
+		<?php echo form_password($password); ?>
+		<?php echo form_error($password['name']); ?>
+	</div>
+	<div class="form-group">
+		<?php echo form_label('Confirm Password', $confirm_password['id']); ?>
+		<?php echo form_password($confirm_password); ?>
+		<?php echo form_error($confirm_password['name']); ?>
+	</div>
+	<div class="form-group">
+		<label>성별</label><br/>
+		<label class="radio-inline">
+			<input type='radio' name='gender' value='f'/> 여
+		</label>
+		<label class="radio-inline">
+			<input type='radio' name='gender' value='m'/> 남
+		</label>
+	</div>
+		
+    <?php
+	$birth=array('year'=>1990,'month'=>8,'day'=>8);
+	?>
+	<div class="form-group">
+		<label>생년월일</label>
+		<div id='birth_field'>
+			<select name='year' class='no-jquery'>
+				<?php for($i=date('Y'); $i>1900; $i--): ?>
+					<option value="<?php echo $i?>"<?if($birth['year']==$i){?> selected<?}?>><?php echo $i?>년</option>
+				<?php endfor;?>
+			</select>
+			<select name='month' class='no-jquery'>
+				<?php for($i=1; $i<13; $i++): ?>
+					<option value="<?php echo $i?>"<?if($birth['month']==$i){?> selected<?}?>><?php echo $i?>월</option>
+				<?php endfor;?>
+			</select>
+			<select name='day' class='no-jquery'>
+				<?php for($i=1; $i<32; $i++): ?>
+					<option value="<?php echo $i?>"<?if($birth['day']==$i){?> selected<?}?>><?php echo $i?>일</option>
+				<?php endfor;?>
+			</select>
+		</div>
+	</div>
 
 	<?php if ($captcha_registration) {
 		if ($use_recaptcha) { ?>
-	<tr>
-		<td colspan="2">
-			<div id="recaptcha_image"></div>
-		</td>
-		<td>
-			<a href="javascript:Recaptcha.reload()">Get another CAPTCHA</a>
-			<div class="recaptcha_only_if_image"><a href="javascript:Recaptcha.switch_type('audio')">Get an audio CAPTCHA</a></div>
-			<div class="recaptcha_only_if_audio"><a href="javascript:Recaptcha.switch_type('image')">Get an image CAPTCHA</a></div>
-		</td>
-	</tr>
-	<tr>
-		<td>
-			<div class="recaptcha_only_if_image">Enter the words above</div>
-			<div class="recaptcha_only_if_audio">Enter the numbers you hear</div>
-		</td>
-		<td><input type="text" id="recaptcha_response_field" name="recaptcha_response_field" /></td>
-		<td style="color: red;"><?php echo form_error('recaptcha_response_field'); ?></td>
+	<div class="form-group">
+		<div id="recaptcha_image"></div>
+		<a href="javascript:Recaptcha.reload()">Get another CAPTCHA</a>
+		<div class="recaptcha_only_if_image"><a href="javascript:Recaptcha.switch_type('audio')">Get an audio CAPTCHA</a></div>
+		<div class="recaptcha_only_if_audio"><a href="javascript:Recaptcha.switch_type('image')">Get an image CAPTCHA</a></div>
+	</div>
+	<div class="form-group">
+		<div class="recaptcha_only_if_image">Enter the words above</div>
+		<div class="recaptcha_only_if_audio">Enter the numbers you hear</div>
+		<input type="text" id="recaptcha_response_field" name="recaptcha_response_field" class="form-control"/>
+		<?php echo form_error('recaptcha_response_field'); ?>
 		<?php echo $recaptcha_html; ?>
-	</tr>
+	</div>
 	<?php } else { ?>
-	<tr>
-		<td colspan="3">
-			<p>Enter the code exactly as it appears:</p>
-			<?php echo $captcha_html; ?>
-		</td>
-	</tr>
-	<tr>
-		<td><?php echo form_label('Confirmation Code', $captcha['id']); ?></td>
-		<td><?php echo form_input($captcha); ?></td>
-		<td style="color: red;"><?php echo form_error($captcha['name']); ?></td>
-	</tr>
+	<div class="form-group">
+		<p>Enter the code exactly as it appears:</p>
+		<?php echo $captcha_html; ?>
+	</div>
+	<div class="form-group">
+		<?php echo form_label('Confirmation Code', $captcha['id']); ?>
+		<?php echo form_input($captcha); ?>
+		<?php echo form_error($captcha['name']); ?>
+	</div>
 	<?php }
 	} ?>
-</table>
-<?php echo form_submit('register', 'Register'); ?>
+	<button type="submit" name="register" class="btn btn-primary">Register</button>
 <?php echo form_close(); ?>
