d81eefab6fbc3a35d6f4cef067c1f7a50a1d2acd
SnooeyNET <sople1@snooey.net>
2013-12-19 16:39:07 +0900

    1

--- app/models/profile_model.php
+++ app/models/profile_model.php
@@ -118,7 +118,7 @@ class profile_model extends CI_Model {
                 limit ?, ?;
                 "; // raw query :)
         $query = $this->db->query($sql, array($params->user_id, ((($params->page)-1)*$params->delimiter), $params->delimiter));
-        
+        var_export($this->db->last_query());
         foreach($query->result() as $row)
             var_export($row);
         exit();
