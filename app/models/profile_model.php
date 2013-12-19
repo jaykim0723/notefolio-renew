3d32ee6e033b1e49a83f719e7dc0cca29c22eabb
SnooeyNET <sople1@snooey.net>
2013-12-19 17:14:57 +0900

    1

--- app/models/profile_model.php
+++ app/models/profile_model.php
@@ -195,29 +195,39 @@ class profile_model extends CI_Model {
                 $params->{$key} = $value;
         }
 
-        /* 성수씨 부탁해요~ 
-        $this->db
-            ->select('works.*, users.*, users.user_id as user_id')
-            // ->select('work_id, title, realname, regdate, keywords, tags, user_id, folder, contents, moddate, hit_cnt, note_cnt, comment_cnt, collect_cnt, ccl, discoverbility')
-            ->from('works')
-            ->join('users', 'users.id = works.user_id', 'left')
-            ->limit($params->delimiter, ((($params->page)-1)*$params->delimiter)); //set
-        $works = $this->db->get();
-        */
-
+        $sql = "SELECT list.follower_id,
+                     if(isnull(now_following.follow_id), 0, 1) as is_follow,
+                     following_users.*
+                from user_follows list
+                left join (
+                    select users.id as user_id, users.username, users.email,
+                     users.realname, users.created, users.modified,
+                     user_profiles.keywords
+                    from users
+                    left join user_profiles on users.id = user_profiles.user_id
+                ) following_users on list.follower_id = following_users.user_id
+                left join (
+                    select follow_id
+                    from user_follows
+                    where follower_id = ?
+                ) now_following on list.follower_id = now_following.follow_id
+                where list.follow_id = ?
+                order by list.id desc
+                limit ?, ?;
+                "; // raw query :)
+        $query = $this->db->query($sql, array(USER_ID, $params->user_id, ((($params->page)-1)*$params->delimiter), $params->delimiter));
+        
         $rows = array();
-        // foreach ($works->result() as $row)
-        for ($i=0; $i<$params->delimiter; $i++) // 일단 dummy 정보를 이용한다.
+        foreach($query->result() as $row)
         {
             // 값을 조작해야할 필요가 있을 때에는 여기에서 한다
             $row = (object)array(
-                'user_id'         => 234, 
-                'username'   => 'maxzidell',
-                'email'      => 'zidell@gmail.com',
-                'level'      => 2,
-                'realname'   => '이흥현',
-                'created'    => '2012-01-02 10:20:10',
-                'modified'   => '2013-08-01 11:11:11', // profile 사진 갱신을 위해서 필요하다.
+                'user_id'    => $row->user_id,
+                'username'   => $row->username,
+                'email'      => $row->email,
+                'realname'   => $row->realname,
+                'created'    => $row->created,
+                'modified'   => $row->modified, // profile 사진 갱신을 위해서 필요하다.
                 'keywords' => array(
                     '파인아트', '동영상', 'UI/UX'
                 ),
@@ -243,7 +253,7 @@ class profile_model extends CI_Model {
                         'modified' => '2013-08-01 11:11:11'                        
                     )
                 ),
-                'is_follow' => (rand(0,1)==1 ? 'y' : 'n') // 기존에 어떤명을 했는지 잘 기억이...
+                'is_follow' => ($row->is_follow==1 ? 'y' : 'n') // 기존에 어떤명을 했는지 잘 기억이...
             );
             $rows[] = $row;
         }
