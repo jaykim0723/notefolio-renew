0346e4aba357c381786c3605a407a6eb11b78850
zidell <zidell@gmail.com>
2014-01-02 22:13:35 +0900

    이제 정상

--- www/log-2014-01-02 21:57:15.php
+++
@@ -1,7 +0,0 @@
-SELECT *
-FROM (`ci_sessions`)
-WHERE `session_id` =  '330185942e42270fc081ff1966fd6c4a'
-AND `user_agent` =  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'
-INSERT INTO `log_access` (`useragent`, `to_access`, `is_mobile`, `is_robot`, `referer`, `remote_addr`, `regdate`) VALUES ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36', '/feed/check_unread', 'N', 'N', 'http://notefolio_renew.localhost/toekki/101720?', '127.0.0.1', '2014-01-02 21:57:15')
-===[took:0.005]
-
--- www/log-2014-01-02 22:06:44.php
+++
@@ -1,8 +0,0 @@
-SELECT *
-FROM (`ci_sessions`)
-WHERE `session_id` =  '9c05a7408814b65575524c8cf47840ee'
-AND `user_agent` =  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'
-UPDATE `ci_sessions` SET `last_activity` = 1388668004, `session_id` = '8ef98d44c15e6aed227edc1680234b47' WHERE session_id = '9c05a7408814b65575524c8cf47840ee'
-INSERT INTO `log_access` (`useragent`, `to_access`, `is_mobile`, `is_robot`, `referer`, `remote_addr`, `regdate`) VALUES ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36', '/feed/check_unread', 'N', 'N', 'http://notefolio_renew.localhost/toekki/101720?', '127.0.0.1', '2014-01-02 22:06:44')
-===[took:0.01]
-
