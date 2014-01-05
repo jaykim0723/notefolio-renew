5053abf7ceb29995b91ca94124a808065edaad32
SnooeyNET <sople1@snooey.net>
2014-01-05 16:58:31 +0900

    1

--- app/models/user_model.php
+++ app/models/user_model.php
@@ -189,25 +189,32 @@ class user_model extends CI_Model {
             return $data;
         }
 
-        # 성수씨 
-        $user->keywords = array( // temporary
-            '파인아트', '모션그래픽', '동영상'
-        );
-        $user->sns = (object)array( // temporary
-            'facebook' => 'maxzidell',
-            'twitter' => 'maxzidell'
-        );
+        if(count($user)<1){
+            $data = (object)array(
+                'status' => 'fail',
+                'message' => 'no_data'
+            );
+        }
+        else {
+            # 성수씨 
+            $user->keywords = array( // temporary
+                '파인아트', '모션그래픽', '동영상'
+            );
+            $user->sns = (object)array( // temporary
+                'facebook' => 'maxzidell',
+                'twitter' => 'maxzidell'
+            );
 
-
-        unset($user->password);
-        unset($user->new_password_key);
-        unset($user->new_password_requested);
-        unset($user->new_email_key);
-        unset($user->new_email);
-        $data = (object)array(
-            'status' => 'done',
-            'row' => $user
-        );
+            unset($user->password);
+            unset($user->new_password_key);
+            unset($user->new_password_requested);
+            unset($user->new_email_key);
+            unset($user->new_email);
+            $data = (object)array(
+                'status' => 'done',
+                'row' => $user
+            );
+        }
 
     	return $data;
     }
