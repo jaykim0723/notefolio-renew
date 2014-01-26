52488d1fedd0e11f9a3644031ac35d1309216417
Yoon, Seongsu <sople1@snooey.net>
2014-01-26 18:30:07 +0900

    1

---
+++ app-cli/controllers/migrate.php
@@ -0,0 +1,40 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * @brief migrate(original) Controller
+ *
+ * @author Yoon, Seongsu(soplel@snooey.net)
+ */
+ 
+class migrate extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 */
+	public function index($to='null')
+	{
+		echo "Hello {$to}!".PHP_EOL;
+	}
+    
+	/**
+	 * migrate info for user list
+	 *
+	 * @param int $user_id
+	 */
+	public function get_user(){
+        //-- facebook post 
+        $cmd = 'php '.$this->input->server('DOCUMENT_ROOT').'../../notefolio-web/app_cli/cli.php migrate user_list';
+        var_export(exec($cmd));  
+	}
+
+	/**
+	 * migrate info for work list
+	 *
+	 */
+	public function get_work(){
+	}
+
+}
+
+/* End of file migrate.php */
+/* Location: ./application/controllers/migrate.php */
