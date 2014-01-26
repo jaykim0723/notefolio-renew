67fa3b02a98c3577f09306dc35eda06b19a46220
Yoon, Seongsu <sople1@snooey.net>
2014-01-26 19:56:15 +0900

    1

--- app-cli/controllers/migrate.php
+++ app-cli/controllers/migrate.php
@@ -142,7 +142,7 @@ class migrate extends CI_Controller {
             $new[] = $new_keyword;
         }
         sort($new);
-        $new = array_unique($new)
+        $new = array_unique($new);
 
         return implode('', $new);
     }
