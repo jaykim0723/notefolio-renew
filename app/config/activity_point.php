b1cb00cd24b803bda0b5afedc31947fa9c0fcccc
Yoon, Seongsu <sople1@snooey.net>
2014-01-29 15:02:01 +0900

    q

---
+++ app/config/activity_point.php
@@ -0,0 +1,35 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Activity Point Config File
+ * @author Yoon, Seongsu(sople1@snooey.net)
+ * 
+ */
+ 
+ $config['ap'] = array(
+ 	'user' => array(
+ 			'create'			=> 0,
+ 			'update'			=> 0,
+ 			'delete' 			=> 0,
+
+ 			'follow' 			=> 0,
+
+ 			'hot_creators'	 	=> 0,
+
+ 			'connect_facebook' 	=> 0,
+
+ 			'profile_face'		=> 0,
+ 			'profile_background'=> 0,
+
+ 			'folder'			=> 0,
+ 		),
+ 	'work' => array(
+ 			'create'			=> 0,
+ 			'update'			=> 0,
+ 			'delete'			=> 0,
+
+ 			'view'				=> 0,
+ 			'comment'			=> 0,
+ 			'note'				=> 0,
+ 			'collect'			=> 0,
+ 		),
+ 	);
