c1a192762dfcc7bbc3f3f9bd76d329be9400a694
Yoon, Seongsu <sople1@snooey.net>
2014-01-29 18:52:32 +0900

    1

--- app/views/comment/comment_form_view.php
+++ app/views/comment/comment_form_view.php
@@ -4,7 +4,7 @@
 		<a class="btn btn-nofol btn-cancel-comment"><i class="spi spi-close">close</i></a>
 	</div>
 	<a class="comment-profile-area" href="<?php echo site_url($this->session->userdata('username')) ?>" target="_blank">
-		<img src="<?php echo '/data/profiles/'.$this->session->userdata('username').'.jpg' ?>" alt="">
+		<img src="<?php echo '/data/profiles/'.$this->session->userdata('username').'_face.jpg' ?>" alt="">
 		<i class="si si-face-medium"></i>
 	</a>
 	<textarea name="content" class="comment-textarea"></textarea>
