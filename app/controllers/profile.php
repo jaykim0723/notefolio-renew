d19b1b3e557043a43048918cd9b3b3db0f059093
Yoon, Seongsu <sople1@snooey.net>
2014-01-29 15:27:08 +0900

    1

--- app/controllers/profile.php
+++ app/controllers/profile.php
@@ -317,7 +317,6 @@ class Profile extends CI_Controller {
 		));
 		if(!$this->input->is_ajax_request())
 			$this->layout->set_view('profile/header_view', $user);
-		exit('AAAAA');
 		$this->layout->set_view('profile/collection_listing_view', $collection_list)->render();
 	}
 
