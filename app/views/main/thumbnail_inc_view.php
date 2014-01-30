4408f6af9990a56400d7f86271255b3a8e6bc685
zidell <zidell@gmail.com>
2014-01-30 18:13:06 +0900

    aoeu

--- app/views/main/thumbnail_inc_view.php
+++ app/views/main/thumbnail_inc_view.php
@@ -3,7 +3,7 @@ $wide = in_array($row->key, array(4,11));
 ?>
 <li class="thumbbox infinite-item <?php echo $wide ? 'wide' : '' ?>">
 	<a class="go-to-work-info" href="/<?php echo $row->user->username ?>/<?php echo $row->work_id ?>">
-		<img src="/img/thumb<?php echo $wide ? '_wide' : '' ?><?php echo rand(0,7) ?>.jpg"/>
+		<img src="/data/covers/<?php echo $row->work_id ?>_t<?php echo $wide ? '3' : '2' ?>.jpg"/>
 		<span class="main-work-info si-main-info-bg" style="">
 			<i class="spi spi-view">Hit</i> <?php echo $row->hit_cnt ?>
 			<i class="spi spi-love">Note</i> <?php echo $row->note_cnt ?>
@@ -12,7 +12,7 @@ $wide = in_array($row->key, array(4,11));
 		<span class="main-work-title">
 			<span class="pull-right">
 				<span class="main-work-face">
-					<img src="http://notefolio.net/profiles/147?h=1385655105" alt=""/>
+					<img src="/data/profiles/<?php echo $row->user->username ?>.jpg?_=" alt=""/>
 					<i class="si si-face-small"></i>
 				</span>
 				<?php echo $row->user->username; ?>
