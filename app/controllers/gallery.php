b8b6e92abac8d3dee16b4657099df73cde94e95e
Yoon, Seongsu <sople1@snooey.net>
2014-02-02 14:57:12 +0900

    1

--- app/controllers/gallery.php
+++ app/controllers/gallery.php
@@ -147,6 +147,10 @@ class Gallery extends CI_Controller {
 
 		$json = array(
 			'status'=>($result_t1&&$result_t2&&$result_t3)?'done':'fail',
+			'cropped' => array(
+				't2'=> $to_crop_t2,
+				't3'=> $to_crop_t3
+				),
 			'src'=> array(
 				$this->config->item('temp_upload_uri', 'upload').$work_id.'_t1.jpg?_='.time(),
 				$this->config->item('temp_upload_uri', 'upload').$work_id.'_t2.jpg?_='.time(),
