77109b5c5092c6355fa1b5b1305c6b83dabe295c
Yoon, Seongsu <sople1@snooey.net>
2014-02-03 15:44:31 +0900

    1

--- app/models/profile_model.php
+++ app/models/profile_model.php
@@ -591,25 +591,25 @@ class profile_model extends CI_Model {
             ifnull(sum(cl.count), 0) as collect_cnt
         from
         (
-            select works.work_id, count(distinct t.id) as count
+            select count(distinct t.id) as count
             from works
             left join log_work_view as t on works.work_id = t.work_id
             where works.user_id = ? and t.regdate between ? and ?
         ) v,
         (
-            select works.work_id, count(distinct t.id) as count
+            select count(distinct t.id) as count
             from works
             left join log_work_note as t on works.work_id = t.work_id
             where works.user_id = ? and t.regdate between ? and ?
         ) n,
         (
-            select works.work_id, count(distinct t.id) as count
+            select count(distinct t.id) as count
             from works
             left join work_comments as t on works.work_id = t.work_id
             where
                 works.user_id = ? and t.parent_id = 0 and t.regdate between ? and ?
         ) c,
-        (select works.work_id, count(distinct t.id) as count
+        (select count(distinct t.id) as count
             from works
             left join user_work_collect as t on works.work_id = t.work_id
             where
