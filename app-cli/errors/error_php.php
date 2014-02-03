eed819cc4baa3190812880f24e9a7126cdf58c98
Yoon, Seongsu <sople1@snooey.net>
2014-02-03 19:15:32 +0900

    1

--- app-cli/errors/error_php.php
+++ app-cli/errors/error_php.php
@@ -1,10 +1,6 @@
-<div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;">
+PHP Error!
 
-<h4>A PHP Error was encountered</h4>
-
-<p>Severity: <?php echo $severity; ?></p>
-<p>Message:  <?php echo $message; ?></p>
-<p>Filename: <?php echo $filepath; ?></p>
-<p>Line Number: <?php echo $line; ?></p>
-
-</div>
+Severity: <?php echo $severity; ?>
+Message:  <?php echo $message; ?>
+Filename: <?php echo $filepath; ?>
+Line Number: <?php echo $line; ?>
