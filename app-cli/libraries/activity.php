f4a68e1a64726bcf0ac2c1d41dc002913f32b753
Yoon, Seongsu <sople1@snooey.net>
2014-02-04 11:31:18 +0900

    1

--- app-cli/libraries/activity.php
+++ app-cli/libraries/activity.php
@@ -44,18 +44,19 @@ class Activity {
      */
     
     function make_param($workType, $resource=array())
-    {
-        //-- make work type
+    {   
+        //-- go by work type
         $workType = strtolower($workType);
-        $type_array = array('create','read','update','delete',);
-        if (array_key_exists($workType, $type_array)) {
+        try{
             $resource['workType'] = $workType;
-            return $this->{'make_param_'.$workType};
+            $output =  $this->{'make_param_'.$workType}($resource);
         }
-        else {
+        catch(Exception $e){
             $this->last_error = @json_encode(array('status'=>'fail', 'message'=>'no_have_work_type'));
             return array();
         }
+
+        return $output;
     }
 
     /**
@@ -68,11 +69,8 @@ class Activity {
     
     function make_param_create($params=array())
     {
-        parse_str($params['data'], $opt);
-        var_export($opt);
-
         $data = array();
-        $user_A = $this->ci->user_model->get_info(array('id'=>$opt['user_A']))->row;
+        $user_A = $this->ci->user_model->get_info(array('id'=>$params->data['user_A']))->row;
         $data['user_A'] = array(
             'id'=>$user_A->id,
             'username'=>$user_A->username,
@@ -82,7 +80,7 @@ class Activity {
         switch($params['area']){
             case "user":
                 if(in_array($params['type'], array('follow'))){
-                    $user_B = $this->ci->user_model->get_info(array('id'=>$opt['user_B']))->row;
+                    $user_B = $this->ci->user_model->get_info(array('id'=>$params->data['user_B']))->row;
                     $data['user_B'] = array(
                         'id'=>$user_B->id,
                         'username'=>$user_B->username,
@@ -93,13 +91,13 @@ class Activity {
             case "work":
                 if(in_array($params['type'], array('work'))){
                     $work = $this->ci->work_model->get_info(array('work_id'=>$work_id))->row;
-                    $opt['user_B'] = $work->user->id;
+                    $params->data['user_B'] = $work->user->id;
                     $data['work'] = array(
                         'work_id' => $work->work_id,
                         'title' => $work->title
                         );
 
-                    $user_B = $this->ci->user_model->get_info(array('id'=>$opt['user_B']))->row;
+                    $user_B = $this->ci->user_model->get_info(array('id'=>$params->data['user_B']))->row;
                     $data['user_B'] = array(
                         'id'=>$user_B->id,
                         'username'=>$user_B->username,
