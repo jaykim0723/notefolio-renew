7f470921518cf8aa126c7eefac3fc82ab6cdb2c8
Yoon, Seongsu <sople1@snooey.net>
2014-02-04 19:36:34 +0900

    1

--- app/models/feed_model.php
+++ app/models/feed_model.php
@@ -70,7 +70,7 @@ class feed_model extends CI_Model {
         $this->load->model('alarm_model');
         $user_id = (empty($user_id))?USER_ID:$user_id;
 
-        $feed_count = $this->get_count(array('user_id'=>$user_id));\
+        $feed_count = $this->get_count(array('user_id'=>$user_id));
         $alarm_count = $this->alarm_model->get_count(array('user_id'=>$user_id));
 
         $data = (object)array(
