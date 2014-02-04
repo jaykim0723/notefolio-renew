e7d2ee3508012b183f2ea1f84e15f320db660499
Yoon, Seongsu <sople1@snooey.net>
2014-02-04 12:36:50 +0900

    1

--- app/controllers/gallery.php
+++ app/controllers/gallery.php
@@ -54,7 +54,6 @@ class Gallery extends CI_Controller {
                 }
             }
         }
-        exit(var_export($work));
         $this->layout->set_header(array(
             'keywords' => @implode(', ',$work->row->tags),
             'description' => $description,
