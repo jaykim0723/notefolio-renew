c0141a7f354e5c014257bd2df562d9fc8413b5a0
Yoon, Seongsu <sople1@snooey.net>
2014-02-07 11:35:34 +0900

    1

--- app/views/gallery/form_view.php
+++ app/views/gallery/form_view.php
@@ -4,7 +4,6 @@
 	?>;
 	NFview.keywords = <?php echo json_encode($this->nf->category_to_array($row->keywords, TRUE)); ?>;
 	NFview.area = 'work-form';
-	keywords = $this->nf->category_to_array($row->keywords, TRUE);
 </script>
 
 <?php echo form_open('/gallery/save', array('id'=>'gallery_form', 'role'=>'form')); ?>
@@ -27,7 +26,7 @@
 					$keyword_list = $this->config->item('keyword', 'keyword');
 
 					foreach ($keyword_list as $key => $keyword) { ?>
-						<option value="<?php echo $key?>"<?=(in_array($key, $keywords))?' selected':''?>><?php echo $keyword;?></option>
+						<option value="<?php echo $key?>"<?=(in_array($key, $this->nf->category_to_array($row->keywords, TRUE)))?' selected':''?>><?php echo $keyword;?></option>
 					<?php }	?>
 				</select>
 
