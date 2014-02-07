8bad1a03ebb876998a00b0d5af8257f39d17ba49
Hye Ryoung Kim <hyeryoung.mica.kim@gmail.com>
2014-02-07 18:40:22 +0900

    프로필수정

--- app/views/gallery/info_view.php
+++ app/views/gallery/info_view.php
@@ -35,6 +35,7 @@
 							<div class="centered">
 								<a href="javascript:;" data-id="<?php echo $row->user_id ?>" class="btn btn-follow btn-nofol2 btn-hover <?php echo $row->is_follow=='y'?'activated' : '' ?>" style="border:none;">
 									<i class="spi spi-follow_point"></i>
+									<i class="spi spi-follow_white"></i>
 									<span>Follow<?php echo $row->is_follow=='y'?'ing' : '' ?></span>
 								</a>
 							</div>
