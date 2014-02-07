ec07beb91a5bf2f26ea90d11c70f466bad4cd17b
Yoon, Seongsu <sople1@snooey.net>
2014-02-07 16:09:09 +0900

    1

--- app/views/layout/footer_inc_view.php
+++ app/views/layout/footer_inc_view.php
@@ -1,4 +1,66 @@
+<nav id="mobile-menu" class="hidden-md hidden-lg">
 
+	<ul>
+		<li>
+			<form action="gallery/listing/1" role="form">
+				<div class="input-group input-group-md">
+					<input type="search" name="q" placeholder="search" class="form-control">
+					<div class="input-group-btn">
+						<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+					</div>					
+				</div>
+			</form> 
+		</li>
+		<?php if (USER_ID==0): ?>
+			<li>
+				<a href="/auth/login">Login</a>
+			</li>
+			<li>
+				<a href="/auth/register">Register</a>
+			</li>
+		<?php else: ?>
+			<li id="mobile-menu-profile" class="centered">
+				<a href="/<?php echo $this->nf->get('user')->username ?>">
+					<img src="/data/profiles/<?php echo $this->nf->get('user')->username ?>_face.jpg?_=<?php echo substr($this->nf->get('user')->modified,-2) ?>" alt="" onerror="this.src='/img/default_profile_face.png'">
+				</a>
+				<?php echo $this->nf->get('user')->username ?>
+				<br/>
+				followers <?php echo $this->nf->get('user')->following_cnt ?>
+				/
+				followings <?php echo $this->nf->get('user')->follower_cnt ?>
+			</li>
+			<li>
+				<a class="clear-list" href="/alarm/listing">
+					Alarm
+					<span class="label label-danger rounded unread-alarm"></span>
+				</a>
+			</li>
+			<li>
+				<a class="clear-list" href="/feed/listing">
+					Feed
+					<span class="label label-danger rounded unread-feed"></span>
+				</a>
+			</li>
+
+			<li>
+				<a class="clear-list" href="/<?php echo $this->session->userdata('username') ?>/myworks">My works</a>
+			</li>
+			<li>
+				<a class="clear-list" href="/<?php echo $this->session->userdata('username') ?>/statistics">Statistics</a>
+			</li>
+			<li>
+				<a href="/auth/setting">Settings</a>
+			</li>
+			<li id="mobile-menu-signout">
+				<a href="btn btn-default btn-lg">
+					Sign out
+				</a>
+			</li>
+		<?php endif; ?>
+
+	</ul>
+
+</nav>
 
 <script src="/js/libs/bootstrap.min.js"></script>
 <script src="/js/libs/bootstrap-dialog.js"></script>
