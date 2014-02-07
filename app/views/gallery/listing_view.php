6e8e851c17be3e2fc58d1b50358f9799edf47e6c
zidell <zidell@gmail.com>
2014-02-07 16:17:46 +0900

    aoeu

--- app/views/gallery/listing_view.php
+++ app/views/gallery/listing_view.php
@@ -3,7 +3,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				<form action="" class="form-inline" role="form">
+				<form action="" class="form-inline" role="form" id="gallery-search-form">
 
 					<div class="row">
 						
