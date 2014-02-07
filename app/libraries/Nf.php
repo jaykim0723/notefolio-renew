e554d67867aa39f42b211b22fe3afdd04f525436
zidell <zidell@gmail.com>
2014-02-07 17:58:29 +0900

    aoeu

--- app/libraries/Nf.php
+++ app/libraries/Nf.php
@@ -95,7 +95,7 @@ class Nf
         foreach ($sns as $service => $id){
             if(empty($id)) continue;
             $tmp = $this->sns($service, $id);
-            $s .= '<a href="'.$tmp->link.'" class="'.$service.'" data-value="'.$id.'" class="btn-hover"><i class="spi spi-fb"></i></a>';
+            $s .= '<a href="'.$tmp->link.'" class="'.$service.'" data-value="'.$id.'" class="btn-hover"><i class="spi spi-'.$service.'"></i></a>';
         }
         return $s;
     }
