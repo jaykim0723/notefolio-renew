82d276f9394240ae5e479a87e26d67e1d75c7b17
Hye Ryoung Kim <hyeryoung.mica.kim@gmail.com>
2014-02-09 16:18:24 +0900

    3

--- app/views/profile/header_view.php
+++ app/views/profile/header_view.php
@@ -55,7 +55,7 @@
 
 			<?php if (USER_ID!=$row->user_id): ?>
 			<div class="centered">
-				<a href="javascript:;" data-id="<?php echo $row->user_id ?>" class="btn btn-follow btn-nofol btn-hover <?php echo $row->is_follow=='y'?'activated' : '' ?>" style="border:none;">
+				<a href="javascript:;" data-id="<?php echo $row->user_id ?>" class="btn btn-follow btn-nofol2 btn-hover <?php echo $row->is_follow=='y'?'activated' : '' ?>" style="border:none;">
 					<i class="spi spi-following_white"></i>
 					<i class="spi spi-follow"></i>
 					<span>Follow<?php echo $row->is_follow=='y'?'ing' : '' ?></span>
