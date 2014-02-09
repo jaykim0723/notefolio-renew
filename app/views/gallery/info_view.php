30b8cf5986519b193bbbadcee21e34454f6d1946
zidell <zidell@gmail.com>
2014-02-09 16:01:12 +0900

    aoeu

--- app/views/gallery/info_view.php
+++ app/views/gallery/info_view.php
@@ -63,7 +63,7 @@
 					<div class="work-wrapper infinite-item" id="work-<?php echo $row->work_id ?>" data-id="<?php echo $row->work_id ?>" data-noted="<?php echo $row->noted; ?>" data-collected="<?php echo $row->collected; ?>" data-moddate="<?php echo substr($row->regdate, -2); ?>">
 						<div class="work-small-profile bg1 visible-xs visible-sm">
 							<img src="/data/profiles/<?php echo $row->user->username ?>_face.jpg"/>
-							<h2><?php echo $row->user->username ?></h2>
+							<h2><a href="<?php echo site_url($row->user->username) ?>"><?php echo $row->user->username ?></a></h2>
 							<span><?php echo $this->nf->category_to_string($row->user->user_keywords, true); ?></span>
 							<div class="work-url" style="display:none;"><?php echo site_url($row->user->username.'/'.$row->work_id); ?></div>
 						</div>
