76aa011f44a4c6f796a08bbade4be824a1ea0fda
Hye Ryoung Kim <hyeryoung.mica.kim@gmail.com>
2014-02-09 21:54:05 +0900

    헤

--- app/views/layout/header_view.php
+++ app/views/layout/header_view.php
@@ -87,7 +87,7 @@
 						<a href="/random"><i class="spi spi-refresh">random</i></a>	<!-- 랜덤페이지로 -->
 					<?php elseif(in_array($this->uri->rsegment(1), array('profile'))): // 여기가 프로필페이지나 특정 작가의 상세정보페이지라면 ?>
 						<?php if ($this->session->userdata('username')!=$profile['username']): // 작가 본인이 아닐 때에만 ?>
-							<a href="javascript:;" class="btn btn-follow" style="padding-top: 0;margin-top: -1px;"><i class="spi <?=($profile['is_follow']=='y')?'spi-following':'spi-follow'?>"></i></a> <!-- 이 작가의 팔로우 버튼 -->
+							<a href="javascript:;" class="btn btn-follow" style="padding: 0;margin-top: -1px;"><i class="spi <?=($profile['is_follow']=='y')?'spi-following':'spi-follow'?>"></i></a> <!-- 이 작가의 팔로우 버튼 -->
 						<?php endif ?>
 					<?php endif;?>
 				</div>
