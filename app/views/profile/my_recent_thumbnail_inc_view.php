38c1a8f3ad6f514f4d8decacc705966cdc8fe789
zidell <zidell@gmail.com>
2014-02-10 11:06:38 +0900

    aoeu

--- app/views/profile/my_recent_thumbnail_inc_view.php
+++ app/views/profile/my_recent_thumbnail_inc_view.php
@@ -1,6 +1,6 @@
 <li id="work-recent-<?php echo $row->work_id ?>" class="<?php echo $row->status ?>">
 	<a href="/<?php echo $row->user->username ?>/<?php echo $row->work_id ?>" class="ellipsis">
-		<img src="/data/covers/<?php echo $row->work_id; ?>_t1.jpg?_=<?php echo substr($row->moddate, -3) ?>"/>
+		<img src="/data/covers/<?php echo $row->work_id; ?>_t2.jpg?_=<?php echo substr($row->moddate, -3) ?>"/>
 		<?php echo htmlentities($row->title, ENT_COMPAT, 'utf-8'); ?>
 	</a>
 </li>
