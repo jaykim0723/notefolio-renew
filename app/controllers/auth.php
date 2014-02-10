5a96cd7d6ad1e0fc187cbfdf4462971ae837d48b
Yoon, Seongsu <sople1@snooey.net>
2014-02-10 17:46:28 +0900

    1

--- app/controllers/auth.php
+++ app/controllers/auth.php
@@ -438,8 +438,8 @@ class Auth extends CI_Controller
             $username_useable = $this->users->is_username_available($data['form']['username']);
 
             if(!$username_useable){
+                $data['errors'] = array('username' => "↑ '{$data['form']['username']}'는 이미 사용 중입니다. 다른 문구를 입력해 주세요." );
                 $data['form']['username'] = $this->session->userdata('username');
-                $data['errors'] = array('username' => "↑ {$errors['login']}" );
             }
 
         }
