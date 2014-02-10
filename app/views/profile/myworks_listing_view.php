aaf36d97e0a6532c5f0f155e5e59e6417bc787f6
Hye Ryoung Kim <hyeryoung.mica.kim@gmail.com>
2014-02-10 11:50:14 +0900

    리스팅

--- app/views/profile/myworks_listing_view.php
+++ app/views/profile/myworks_listing_view.php
@@ -1,6 +1,6 @@
 <?php if (!$this->input->is_ajax_request()): ?> 
 
-<section class="listing" style="padding-top:20px;">
+<section class="listing" style="margin-top:20px;">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
