b39f880a7c54bb77a593c6b2624b54f7d2ffa0e1
zidell <zidell@gmail.com>
2014-02-10 16:14:24 +0900

    aoeuaoeuaoeuoaeu

--- app/views/profile/thumbnail_inc_view.php
+++ app/views/profile/thumbnail_inc_view.php
@@ -1,5 +1,6 @@
 <li class="thumbbox infinite-item <?php echo $row->status ?>">
 	<a href="/<?php echo $row->user->username ?>/<?php echo $row->work_id ?>">
+		<?php echo htmlentities($row->title, ENT_COMPAT, 'utf-8'); ?>
 		<img src="/data/covers/<?=($row->work_id)?>_t1.jpg"/>
 	</a>
 </li>
