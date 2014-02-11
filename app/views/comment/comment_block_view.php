e145643e083a96ad169cc00e09e7d8b987a12ebf
zidell <zidell@gmail.com>
2014-02-11 15:26:04 +0900

    oaeu

--- app/views/comment/comment_block_view.php
+++ app/views/comment/comment_block_view.php
@@ -29,7 +29,7 @@
 			<?php endif; ?>
 		</div>
 		<a class="comment-profile-area" target="_blank" href="<?php echo site_url($row->user->username); ?>">
-			<img src="<?php echo site_url('data/profiles/'.$row->user->username) ?>_face.jpg" alt="">
+			<img src="<?php echo site_url('data/profiles/'.$row->user->username) ?>_face.jpg?_=<?php echo substr($row->user->modified,-2) ?>" alt="" onerror="this.src='/img/default_profile_face.png'"/>
 			<i class="si si-face-medium"></i>
 		</a>
 		<div class="comment-username">
