b96e8c4256173178b3a7a08953b67691b1c91193
Yoon, Seongsu <sople1@snooey.net>
2014-02-11 17:50:48 +0900

    1

--- app/models/work_model.php
+++ app/models/work_model.php
@@ -265,6 +265,7 @@ class work_model extends CI_Model {
                     ->select('work_id')
                     ->where('work_id >', $data->row->work_id)
                     ->where('user_id', $user->id)
+                    ->where('works.status !=', 'deleted');
                     ->order_by('work_id', 'asc')
                     ->limit(1)
                     ->get('works')->row();
@@ -285,6 +286,7 @@ class work_model extends CI_Model {
                     ->select('work_id')
                     ->where('work_id <', $data->row->work_id)
                     ->where('user_id', $user->id)
+                    ->where('works.status !=', 'deleted');
                     ->order_by('work_id', 'desc')
                     ->limit(1)
                     ->get('works')->row();
