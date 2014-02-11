0f35ea82b0deb796c25e6c67866e72c9589c6f3d
Yoon, Seongsu <sople1@snooey.net>
2014-02-11 09:59:43 +0900

    1

--- app/models/work_model.php
+++ app/models/work_model.php
@@ -108,6 +108,16 @@ class work_model extends CI_Model {
             case "featured":
                 $this->db->order_by('nofol_rank', 'desc');
             break;
+            case "nofol_rank":
+                $this->db->join('(SELECT
+                    ref_id as work_id,
+                    ifnull(sum(point_get), 0) as point 
+                    FROM `notefolio-renew`.log_activity
+                    where area=\'work\' 
+                    group by work_id) feedback_point', 'works.work_id = feedback_point.work_id', 'left')
+                $this->db->select('(  + feedback_point.point +  ) as rank_point', FALSE)
+                $this->db->order_by('rank_point', 'desc');
+            break;
     		default:
     			if(is_array($params->order_by))
     				$this->db->order_by($params->order_by);
