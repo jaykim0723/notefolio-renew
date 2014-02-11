ac7b5473886706470b10cbcaa63bfc2798f23058
Yoon, Seongsu <sople1@snooey.net>
2014-02-11 18:14:41 +0900

    1

--- app/controllers/acp/work.php
+++ app/controllers/acp/work.php
@@ -68,18 +68,16 @@ class work extends CI_Controller {
                 $limit = array($args['page'], $args['delimiter']);
 
                 $data['list'] = $this->db
-                    ->select('works.*')
-                    ->select('users.id as user_id, users.username as user_username, users.realname as user_realname')
+                    ->select('works.*, users.id as user_id, users.username as user_username, users.realname as user_realname')
                     ->limit($limit[1],($limit[0]-1)*$limit[1])
                     ->join('users', 'works.user_id=users.id', 'left')
                     ->join('user_profiles', 'users.id=user_profiles.user_id', 'left')
                     ->get('works')->result();
-                echo($this->db->last_query());
 
                 foreach($data['list'] as $key=>$val){
                     $user = new stdClass();
-                    var_export($val);
                     foreach($val as $sub_key =>$sub_val){
+                        var_export($sub_key);
                         if(strpos('user_', $sub_key)){
                             echo str_replace('user_', '', $sub_val);
                             $user->{str_replace('user_', '', $sub_key)} = $sub_val; 
