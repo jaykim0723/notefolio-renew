73377262d0a796b53ddc903e35042d94000eb89d
Hye Ryoung Kim <hyeryoung.mica.kim@gmail.com>
2014-02-11 17:53:59 +0900

    info

--- app/views/layout/header_auth_view.php
+++ app/views/layout/header_auth_view.php
@@ -3,6 +3,9 @@
 <div id="auth-wrapper">
 	<header id="auth-header">
 		<a href="/"><i class="pi pi-nflogo2">nflogo2</i></a>
+
+		<div class="bg-info"><i class="spi spi-work_white">work_white</i>artist 김현영 / title 신발 신은 흰둥이</div>
+
 	</header>
 
 
