23fa52d995746e3d78697d91df7a35bbb1bb27d4
Yoon, Seongsu <sople1@snooey.net>
2014-02-11 19:23:47 +0900

    1

--- app/views/acp/work_works_list.php
+++ app/views/acp/work_works_list.php
@@ -49,7 +49,7 @@
             스탭포인트 수정: <br/>
             <input type="text" value="<?=$v->staffpoint?>" name="staffpoint" style="width: 4em;"  />
             <a>
-              <span>수정</span>
+              <span class="btn btn-info">수정</span>
             </a>
           </td>
           <td><?=$v->regdate?></td>
