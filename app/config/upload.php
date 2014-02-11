62c80294715c6ca340b0c135bf8132f6e42c875b
Yoon, Seongsu <sople1@snooey.net>
2014-02-11 13:56:44 +0900

    1

--- app/config/upload.php
+++ app/config/upload.php
@@ -27,8 +27,8 @@ $config['thumbnail_large']=array('max_width'  => 1600, //pixel
 $config['thumbnail_medium']=array('max_width' => 800,  //pixel
                                 'max_height'  =>   0  //pixel, not using
                                 );
-$config['thumbnail_small']= array('max_width' => 400,  //pixel
-                                'max_height'  => 400  //pixel
+$config['thumbnail_small']= array('max_width' => 360,  //pixel
+                                'max_height'  => 360  //pixel
                                 );
 
 $config['thumbnail_wide']= array('max_width'  => 760,  //pixel
