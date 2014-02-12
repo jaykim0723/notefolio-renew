0076bae641af0aafdc9e5aae2785984a7378623a
Hye Ryoung Kim <hyeryoung.mica.kim@gmail.com>
2014-02-12 22:51:25 +0900

    pass

--- app/views/auth/change_password_form.php
+++ app/views/auth/change_password_form.php
@@ -9,29 +9,29 @@ echo form_open('', array(
 	'submitting' => 1
 ));
 ?>
-
+<div class="biggroup">
 	<div class="form-group <?php echo isset($errors['old_password']) ? 'error' : ''?>">
-		<label for="">현재 비밀번호</label>
+		<label for="" class="labeltext">현재 비밀번호</label>
 		<input type="password" name="old_password" class="form-control" id="old_password" value="<?php echo set_value('old_password') ?>"/>
 		<div class="form-error"><?php echo isset($errors['old_password']) ? '↑ '.$errors['old_password'] : '' ?></div>
 	</div>
 
 	<div class="form-group <?php echo isset($errors['new_password']) ? 'error' : ''?>">
-		<label for="">새로운 비밀번호</label>
+		<label for="" class="labeltext">새로운 비밀번호</label>
 		<input type="password" name="new_password" class="form-control" id="new_password" value="<?php echo set_value('new_password') ?>" maxlength="<?php echo $this->config->item('password_max_length', 'tank_auth') ?>"/>
 		<div class="form-error"><?php echo isset($errors['new_password']) ? '↑ '.$errors['new_password'] : '' ?></div>
 	</div>
 
 	<div class="form-group <?php echo isset($errors['confirm_new_password']) ? 'error' : ''?>">
-		<label for="">새로운 비밀번호 확인</label>
+		<label for="" class="labeltext">새로운 비밀번호 확인</label>
 		<input type="password" name="confirm_new_password" class="form-control" id="confirm_new_password" value="<?php echo set_value('confirm_new_password') ?>" maxlength="<?php echo $this->config->item('password_max_length', 'tank_auth') ?>"/>
 		<div class="form-error"><?php echo isset($errors['confirm_new_password']) ? '↑ '.$errors['confirm_new_password'] : '' ?></div>
 	</div>
-
+</div>
 
 	<div class='center'>
-		<br/>
-		<button type="submit" id='btnSubmit' class='btn btn-primary btn-block pure-button-big pure-button'>Submit</button>
+	
+		<button type="submit" id='btnSubmit' class='btn btn-darkgray btn-block pure-button-big pure-button'>Submit</button>
 	</div>
 	
 <?php echo form_close(); ?>
