4984de189c2e0f9cbf42674587b3bf98dd7ec4cf
Hye Ryoung Kim <hyeryoung.mica.kim@gmail.com>
2014-02-12 22:49:37 +0900

    email

--- app/views/auth/change_email_form.php
+++ app/views/auth/change_email_form.php
@@ -1,3 +1,4 @@
+
 <?php
 echo form_open('', array(
 	'name' => 'change_email_form',
@@ -8,22 +9,24 @@ echo form_open('', array(
 	'submitting' => 1
 ));
 ?>
-
+<div class="biggroup">
 	<div class="form-group <?php echo isset($errors['password']) ? 'error' : ''?>">
-		<label for="">비밀번호</label>
+		<label for="" class="labeltext">비밀번호</label>
 		<input type="password" name="password" class="form-control" id="password" value=""/>
 		<div class="form-error"><?php echo isset($errors['password']) ? '↑ '.$errors['password'] : '' ?></div>
 	</div>
 
 	<div class="form-group <?php echo isset($errors['email']) ? 'error' : ''?>">
-		<label for="">변경할 이메일 주소</label>
+		<label for="" class="labeltext">변경할 이메일 주소</label>
 		<input type="text" name="email" class="form-control" id="email" value="<?php echo set_value('email') ?>" maxlength="80">
 		<div class="form-error"><?php echo isset($errors['email']) ? '↑ '.$errors['email'] : '' ?></div>
 	</div>
+</div>
 
 	<div class='center'>
-		<br/>
-		<button type="submit" id='btnSubmit' class='btn btn-primary btn-block pure-button-big pure-button'>Submit</button>
+		
+		<button type="submit" id='btnSubmit' class='btn btn-darkgray btn-block pure-button-big pure-button'>Submit</button>
 	</div>
+
 
 <?php echo form_close(); ?>
