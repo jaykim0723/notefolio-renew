6209539ae99e2165f64d01d2cbd19ff45725c0b9
Yoon, Seongsu <sople1@snooey.net>
2014-02-13 13:37:29 +0900

    1

--- app/controllers/acp/work.php
+++ app/controllers/acp/work.php
@@ -31,7 +31,7 @@ class work extends CI_Controller {
     function works($mode='list')
     {
         if($this->uri->segment(4)==FALSE) redirect('/acp/work/works/list');
-        $args = $this->uri->ruri_to_assoc(5);
+        $args = $this->uri->ruri_to_assoc(4);
         print_r($args);
         exit();
         //var_export($args);
