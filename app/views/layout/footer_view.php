d5e3b378fdb9f914f101528da3323b3f65915400
zidell <zidell@gmail.com>
2014-02-13 13:54:27 +0900

    aoeu

--- app/views/layout/footer_view.php
+++ app/views/layout/footer_view.php
@@ -16,9 +16,9 @@
 			<div class="col-md-4" style="padding-top:3px;">
 				<a target="_blank" href="http://twitter.com/notefoliotwt"><img src="http://magazine.notefolio.net/theme_assets/img/twitter.png" style="width:20px;height:20px;">
 				<span class="footerlink">twitter</span></a>
-				<a target="_blank" href="http://notefolio.tistory.com/"><img src="http://magazine.notefolio.net/theme_assets/img/blog.png" style="width:20px;height:20px;margin-left:15px;">
+				<a target="_blank" href="http://notefolio.tistory.com/"><img src="http://magazine.notefolio.net/theme_assets/img/blog.png" style="width:20px;height:20px;">
 				<span class="footerlink">blog</span></a>
-				<a target="_blank" href="https://www.facebook.com/notefolio"><img src="http://magazine.notefolio.net/theme_assets/img/facebook.png" style="width:20px;height:20px;margin-left:15px;">
+				<a target="_blank" href="https://www.facebook.com/notefolio"><img src="http://magazine.notefolio.net/theme_assets/img/facebook.png" style="width:20px;height:20px;">
 				<span class="footerlink">facebook</span></a>
 			</div>
 		</div>
