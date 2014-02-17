4c2192712fdd5b3cc44b01f48d4d5275ec77520b
Yoon, Seongsu <sople1@snooey.net>
2014-02-17 14:44:48 +0900

    1

--- app/views/alarm/listing_view.php
+++ app/views/alarm/listing_view.php
@@ -13,7 +13,9 @@
 					<?php $this->load->view('feed/activity_inc_view', array('row'=>$row)) ?>
 					<?php endforeach ?>
 				</ul>
+				<?php if(count($rows)>0){ ?>
 				<a href="/alarm/listing/<?php echo $page+1; ?>" id="alarm-more" class="alarm-more-link btn btn-default btn-block btn-alarm-more alarm-height"><i class="spi spi-down"></i><i class="spi spi-down_point"></i>see more</a>
+				<?php } ?>
 <?php if (!$this->input->is_ajax_request()): ?>
 			</div>
 		</div>
