dac104af863cc090bcf47ceca5c7e8c4b0d0dc78
Yoon, Seongsu <sople1@snooey.net>
2014-02-18 12:06:06 +0900

    1

--- app/views/gallery/thumbnail_inc_view.php
+++ app/views/gallery/thumbnail_inc_view.php
@@ -10,8 +10,8 @@
 			<span class="pull-right go-profile-area" data-username="<?php echo $row->user->username ?>">
 				<!-- <span class="pull-right ellipsis" style="margin-top: 6px;margin-left: 5px;"><?php echo $row->user->realname; ?></span> -->
 				<span class="pull-right main-work-face">
-					<img src="/data/profiles/<?=$row->user->username?>_face.jpg?_=<?php echo substr($row->user->modified,-2) ?>" alt="" onerror="this.src='/img/default_profile_face.png'"/>
-					<i class="si si-face-small"></i>   
+					<img class="icon-round" src="/data/profiles/<?=$row->user->username?>_face.jpg?_=<?php echo substr($row->user->modified,-2) ?>" alt="" onerror="this.src='/img/default_profile_face.png'"/>
+					<!--[if lte IE 9]><i class="si si-face-small"></i><![endif]-->   
 				</span>
 			</span>
 			<span class="work-title-char"><?php echo $row->title; ?></span>
