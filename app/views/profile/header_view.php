d7c8fdea13bc7fa37f573bb05afc1f1484dcf382
Yoon, Seongsu <sople1@snooey.net>
2014-02-20 18:15:19 +0900

    1

--- app/views/profile/header_view.php
+++ app/views/profile/header_view.php
@@ -195,7 +195,7 @@ if(!file_exists($this->input->server('DOCUMENT_ROOT').$filename)){
 </script>
 <?php
 	// /app/controller/auth.php line 653
-	//$this->session->set_flashdata('welcome_newmember',true); // 가입환영용
+	$this->session->set_flashdata('welcome_newmember',true); // 가입환영용
 	if($this->session->flashdata('welcome_newmember')){
 ?>
 <script type="text/javascript">
