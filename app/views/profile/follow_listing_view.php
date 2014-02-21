17a16fdde3d9c04c3f5b71daf0b275c746019566
Yoon, Seongsu <sople1@snooey.net>
2014-02-21 18:34:37 +0900

    1

--- app/views/profile/follow_listing_view.php
+++ app/views/profile/follow_listing_view.php
@@ -10,9 +10,9 @@
 				<div class="alert alert-info" style="color:#7a7880;text-align:center;">
 					<?php $is_me = (isset($this->nf->get('user')->username) && $this->nf->get('user')->username == $row->username); ?>
 				  	<?php if($this->uri->segment(2)=="followings"){ ?>
-				  		<strong>아직 <?=($is_me)?$row->realname.'님이 ':'';?>팔로우하는 사람이 없습니다.</strong>
+				  		<strong>아직 <?=($is_me)?'':$row->realname.'님이 ';?>팔로우하는 사람이 없습니다.</strong>
 				  	<?php } else if($this->uri->segment(2)=="followers"){ ?>
-				  		<strong>아직 <?=($is_me)?$row->realname.'님을 ':'나를 ';?>팔로우 하는 사람이 없습니다.</strong>
+				  		<strong>아직 <?=($is_me)'나를 ':?$row->realname.'님을 ';?>팔로우 하는 사람이 없습니다.</strong>
 				  	<?php } else { ?>
 				  		<strong>응? 이걸 어떻게 보셨나요? <a href="/info/contact_us">알려주세요...</a></strong>
 				  	<?php } ?>
