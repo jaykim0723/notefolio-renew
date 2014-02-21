28476ad41e5dfa6c7e4e0e3963a24c8fde92a683
Yoon, Seongsu <sople1@snooey.net>
2014-02-21 17:44:04 +0900

    1

--- app/views/auth/change_password_form.php
+++ app/views/auth/change_password_form.php
@@ -16,13 +16,13 @@ echo form_open('', array(
 		<div class="form-error"><?php echo isset($errors['old_password']) ? '↑ '.$errors['old_password'] : '' ?></div>
 	</div>
 
-	<div class="form-group <?php echo isset($errors['new_password']) ? 'error' : ''?>">
+	<div id="form-new-password" class="form-group <?php echo isset($errors['new_password']) ? 'error' : ''?>">
 		<label for="" class="labeltext">새로운 비밀번호</label>
 		<input type="password" name="new_password" class="form-control" id="new_password" value="<?php echo set_value('new_password') ?>" maxlength="<?php echo $this->config->item('password_max_length', 'tank_auth') ?>"/>
 		<div class="form-error"><?php echo isset($errors['new_password']) ? '↑ '.$errors['new_password'] : '' ?></div>
 	</div>
 
-	<div class="form-group <?php echo isset($errors['confirm_new_password']) ? 'error' : ''?>">
+	<div id="form-confirm-new-password" class="form-group <?php echo isset($errors['confirm_new_password']) ? 'error' : ''?>">
 		<label for="" class="labeltext">새로운 비밀번호 확인</label>
 		<input type="password" name="confirm_new_password" class="form-control" id="confirm_new_password" value="<?php echo set_value('confirm_new_password') ?>" maxlength="<?php echo $this->config->item('password_max_length', 'tank_auth') ?>"/>
 		<div class="form-error"><?php echo isset($errors['confirm_new_password']) ? '↑ '.$errors['confirm_new_password'] : '' ?></div>
@@ -33,5 +33,22 @@ echo form_open('', array(
 	
 		<button type="submit" id='btnSubmit' class='btn btn-darkgray btn-block pure-button-big pure-button'>Submit</button>
 	</div>
+
+<script>
+	$(function(){
+		$('input[type="password"]','#form-confirm-new-password').on('keyup keypress blur change', function(){
+			var val = $(this).val();
+			if(val==$('input[type="password"]','#form-new-password').val()){
+            	$('#form-confirm-new-password').removeClass('error');
+            	$('.form-error','#form-confirm-new-password').text('');
+            }else{
+            	$('#form-confirm-new-password').addClass('error');
+            	$('.form-error','#form-confirm-new-password').text('↑ '+'비밀번호를 똑같이 입력해주세요.');
+            }
+			
+		});
+	});
+
+</script>
 	
 <?php echo form_close(); ?>
