59f1cfb01a6bcb7f440d742905e82833b37d748e
Yoon, Seongsu <sople1@snooey.net>
2014-02-21 19:31:12 +0900

    11

--- app/views/auth/reset_password_form.php
+++ app/views/auth/reset_password_form.php
@@ -13,17 +13,38 @@ $confirm_new_password = array(
 );
 ?>
 <?php echo form_open($this->uri->uri_string()); ?>
-<table>
-	<tr>
-		<td><?php echo form_label('New Password', $new_password['id']); ?></td>
-		<td><?php echo form_password($new_password); ?></td>
-		<td style="color: red;"><?php echo form_error($new_password['name']); ?><?php echo isset($errors[$new_password['name']])?$errors[$new_password['name']]:''; ?></td>
-	</tr>
-	<tr>
-		<td><?php echo form_label('Confirm New Password', $confirm_new_password['id']); ?></td>
-		<td><?php echo form_password($confirm_new_password); ?></td>
-		<td style="color: red;"><?php echo form_error($confirm_new_password['name']); ?><?php echo isset($errors[$confirm_new_password['name']])?$errors[$confirm_new_password['name']]:''; ?></td>
-	</tr>
-</table>
-<?php echo form_submit('change', 'Change Password'); ?>
+<div class="biggroup">
+	<div id="form-new-password" class="form-group <?php echo isset($errors['new_password']) ? 'error' : ''?>">
+		<label for="" class="labeltext">새로운 비밀번호</label>
+		<input type="password" name="new_password" class="form-control" id="new_password" value="<?php echo set_value('new_password') ?>" maxlength="<?php echo $this->config->item('password_max_length', 'tank_auth') ?>"/>
+		<div class="form-error"><?php echo isset($errors['new_password']) ? '↑ '.$errors['new_password'] : '' ?></div>
+	</div>
+
+	<div id="form-confirm-new-password" class="form-group <?php echo isset($errors['confirm_new_password']) ? 'error' : ''?>">
+		<label for="" class="labeltext">새로운 비밀번호 확인</label>
+		<input type="password" name="confirm_new_password" class="form-control" id="confirm_new_password" value="<?php echo set_value('confirm_new_password') ?>" maxlength="<?php echo $this->config->item('password_max_length', 'tank_auth') ?>"/>
+		<div class="form-error"><?php echo isset($errors['confirm_new_password']) ? '↑ '.$errors['confirm_new_password'] : '' ?></div>
+	</div>
+</div>
+
+	<div class='center'>
+	
+		<button type="submit" id='btnSubmit' class='btn btn-darkgray btn-block pure-button-big pure-button'>Submit</button>
+	</div>
+<script>
+	$(function(){
+		$('input[type="password"]','#form-confirm-new-password').on('keyup keypress blur change', function(){
+			var val = $(this).val();
+			if(val==$('input[type="password"]','#form-new-password').val()){
+            	$('#form-confirm-new-password').removeClass('error');
+            	$('.form-error','#form-confirm-new-password').text('');
+            }else{
+            	$('#form-confirm-new-password').addClass('error');
+            	$('.form-error','#form-confirm-new-password').text('↑ '+'비밀번호를 똑같이 입력해주세요.');
+            }
+			
+		});
+	});
+
+</script>
 <?php echo form_close(); ?>
