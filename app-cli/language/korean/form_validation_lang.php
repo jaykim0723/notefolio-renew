e13ec0e6c6e172411dda5fa75b5e600536e39d5e
Yoon, Seongsu <sople1@snooey.net>
2014-02-21 14:08:20 +0900

    1

--- app-cli/language/korean/form_validation_lang.php
+++ app-cli/language/korean/form_validation_lang.php
@@ -1,12 +1,12 @@
 <?php
 
-$lang['required'] 			= "%s 필드가 필요합니다.";
+$lang['required'] 			= "%s 을(를) 입력해주세요.";
 $lang['isset']				= "%s 필드는 반드시 필요한 값입니다.";
-$lang['valid_email']		= "%s 필드는 유효한 이메일 주소를 반드시 포함해야 합니다.";
-$lang['valid_emails'] 		= "%s 필드는 모든 유효한 이메일 주소를 반드시 포함해야 합니다.";
+$lang['valid_email']		= "%s 는 올바른 이메일 형식이 아닙니다.";
+$lang['valid_emails'] 		= "%s 는 올바른 이메일 형식이 아닙니다.";
 $lang['valid_url'] 			= "%s 필드는 유효한 URL을 포함해야 합니다.";
 $lang['valid_ip'] 			= "%s 필드는 유효한 IP를 포함해야 합니다.";
-$lang['min_length']			= "%s 필드의 길이는 최소한 %s 개의 문자를 넘어야 합니다.";
+$lang['min_length']			= "%s 는 %s 자 이상 입력해주세요.";
 $lang['max_length']			= "%s 필드의 길이는 최대 %s 개의 문자를 넘어서는 안됩니다.";
 $lang['exact_length']		= "%s 필드의 길이는 정확히 %s 개의 문자여야 합니다.";
 $lang['alpha']				= "%s 필드는 알파벳 문자만 포함할 수 있습니다.";
@@ -15,7 +15,7 @@ $lang['alpha_dash']			= "%s 필드는 알파벳 문자와 숫자, 밑줄, 대시
 $lang['numeric']			= "%s 필드는 반드시 숫자만 포함할 수 있습니다.";
 $lang['is_numeric']			= "%s 필드는 반드시 숫자만 포함할 수 있습니다.";
 $lang['integer']			= "%s 필드는 반드시 정수만 포함할 수 있습니다.";
-$lang['matches']			= "%s 필드가 %s 필드와 일치하지 않습니다.";
+$lang['matches']			= "%s 이(가) %s 와 같지 않습니다.";
 $lang['is_natural']			= "%s 필드는 반드시 0과 자연수만 포함할 수 있습니다.";
 $lang['is_natural_no_zero']	= "%s 필드는 반드시 0을 초과하는 자연수만 포함할 수 있습니다.";
 $lang['regex_match']			= "%s 필드는 올바른 형식이 아닙니다.";
