cea911fa5820c1324d01f509e9812ad2e3f7f854
Yoon, Seongsu <sople1@snooey.net>
2014-02-21 17:33:16 +0900

    1

--- app/views/auth/change_email_form.php
+++ app/views/auth/change_email_form.php
@@ -16,7 +16,7 @@ echo form_open('', array(
 		<div class="form-error"><?php echo isset($errors['password']) ? '↑ '.$errors['password'] : '' ?></div>
 	</div>
 
-	<div class="form-group <?php echo isset($errors['email']) ? 'error' : ''?>">
+	<div id="form-email" class="form-group <?php echo isset($errors['email']) ? 'error' : ''?>">
 		<label for="" class="labeltext">변경할 이메일 주소</label>
 		<input type="text" name="email" class="form-control" id="email" value="<?php echo set_value('email') ?>" maxlength="80">
 		<div class="form-error"><?php echo isset($errors['email']) ? '↑ '.$errors['email'] : '' ?></div>
@@ -28,5 +28,23 @@ echo form_open('', array(
 		<button type="submit" id='btnSubmit' class='btn btn-darkgray btn-block pure-button-big pure-button'>Submit</button>
 	</div>
 
+<script>
+	$(function(){
+		$('input[type="text"]','#form-email').on('keyup keypress blur change', function(){
+			var val = $(this).val();
+			$.post('/auth/check_email_available', {email: val}, function(data, textStatus, xhr) {
+                var response = $.parseJSON(data);
+                if(response.status=='done'){
+                	$('#form-email').removeClass('error');
+                	$('.form-error','#form-email').text('');
+                }else{
+                	$('#form-email').addClass('error');
+                	$('.form-error','#form-email').text('↑ '+response.error);
+                }
+				
+			});
+		});
+	});
 
+</script>
 <?php echo form_close(); ?>
