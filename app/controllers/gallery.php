64a19a789004a2bf1e37f6d096eadeeff4596d07
Yoon, Seongsu <sople1@snooey.net>
2014-02-24 15:11:46 +0900

    1

--- app/controllers/gallery.php
+++ app/controllers/gallery.php
@@ -246,7 +246,7 @@ class Gallery extends CI_Controller {
         ));
         var_export($input['contents']);
         var_export(str_replace('\\\/', '',$input['contents']));
-        $input['contents'] = json_decode(str_replace('\\\/', '',$input['contents']));
+        $input['contents'] = json_decode(str_replace('\\/', '',$input['contents']));
         $created_images = $deleted_images = array();
         $work_images = $input_images = array();
         exit(var_export($input['contents']));
