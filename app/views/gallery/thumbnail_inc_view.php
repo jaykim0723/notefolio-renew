15e3f720acf6acd46641d9db23116b6e73d1b7d8
Yoon, Seongsu <sople1@snooey.net>
2014-02-24 23:31:17 +0900

    1

--- app/views/gallery/thumbnail_inc_view.php
+++ app/views/gallery/thumbnail_inc_view.php
@@ -8,12 +8,11 @@
 		<em class="pi pi-videoy <?php echo $row->is_video ?>">videoy</em>
 		<span class="main-work-title">
 			<span class="pull-right go-profile-area" data-username="<?php echo $row->user->username ?>">
-				<!-- <span class="pull-right ellipsis" style="margin-top: 6px;margin-left: 5px;"><?php echo $row->user->realname; ?></span> -->
+				<span class="pull-right ellipsis" style="margin-top: 6px;margin-left: 5px;"><?php echo $row->user->realname; ?></span>
 				<span class="pull-right main-work-face">
 					<img class="icon-round" src="/data/profiles/<?=$row->user->username?>_face.jpg?_=<?php echo substr($row->user->modified,-2) ?>" alt="" onerror="this.src='/img/default_profile_face.png'"/>
 					<!--[if lte IE 9]><i class="si si-face-small"></i><![endif]-->   
 				</span>
-				<?=$row->user->realname?>
 			</span>
 			<span class="work-title-char"><?php echo $row->title; ?></span>
 		</span>
