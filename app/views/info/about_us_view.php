f82e5344ceeaf6de74350e021d102fc10d22984e
Yoon, Seongsu <sople1@snooey.net>
2014-02-24 11:31:17 +0900

    1

--- app/views/info/about_us_view.php
+++ app/views/info/about_us_view.php
@@ -5,34 +5,73 @@
         <div class="row">
             <div id="content" class="col-md-9">
                 <div class="well" style="height: 100px;">
-                    소개 이미지
+                    <img src='/img/info/about/1.png' id='title_image'/>
                 </div>
                 <div class="row">
                     <div class="col-md-3">
                         <div class="well" style="height: 300px;">
-                            노폴꾸미기
+                            <img src='/img/info/about/2.png'/>
                         </div>
                     </div>
                     <div class="col-md-9">
                         <div class="well" style="height: 300px;">
-                            노폴 소개글
+note(주목하다) + portfolio(포트폴리오)
+
+크리에이티브 네트워크 노트폴리오는
+여기저기 흩어져 있는 아티스트와 디자이너들이 한 곳에 모여
+자신의 작품을 공개하고 이야기하는 공간입니다.
+
+ㅡ
+
+하나의 창작물이 만들어지기까지는
+수많은 고민과 열정의 시간이 필요합니다.
+하지만 이러한 노력을 들인 작품을 세상에 선보일 기회는 많지 않습니다.
+나와 내 작품을 사람들에게 더 널리 알리고 함께 이야기한다는 것은
+어쩌면 당연한 일입니다.
+<p>
+이 곳에서 여러분의 크리에이티브가 더욱 주목받길 바랍니다.
+</p>
+notefolio.net
                         </div>
                     </div>
                 </div>
                 <div class="well" style="height: 400px;">
-                    소개영상
+                    <iframe src="http://player.vimeo.com/video/58331905" width="100%" height="410" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
                 </div>
 
 
                 <div class="row">
                     <div class="col-md-3">
                         <div class="well" style="height: 300px;">
-                            노폴꾸미기
+                            <img src='/img/info/about/3.png'/>
+                            <br/><br/>
+                            조금 부끄럽지만<br/>
+                            사람들은 노트폴리오에 대해<br/>
+                            이렇게 이야기합니다. 
                         </div>
                     </div>
                     <div class="col-md-9">
                         <div class="well" style="height: 300px;">
-                            노폴 소개글
+                            <div>
+                                <img src='/img/info/about/artist/03_parksumi.jpg' title="박수미(Variety Sum)<br/>많은 작가들과 소통할 수 있는 스크랩 노트 같은 곳. 서로의 의견에 대해 코멘트를 적고 마음에 드는 건 스크랩하는 온라인 노트!"/>
+                                <img src='/img/info/about/artist/04_paeksujung.jpg' title="백수정<br/>아티스트로서 내가 계속 걸어가도록 응원해주는 친구 같아요! 주변의 다른 아티스트들을 바라보게 하고, 나 스스로를 돌아보게 하는 그런 공간이에요!"/>
+                                <img src='/img/info/about/artist/02_kimnoeul.jpg' title="김노을<br/>세상에 이렇게 멋진 작가님들이랑 작품들이 많은 줄 몰랐던 나에게 노폴은? 신세계!"/>
+                                <img src='/img/info/about/artist/01_kwonheesun.jpg' title="권희선(왕구마)<br/>노트폴리오는 갓 지어낸 쌀밥이다. 질리지 않고 담백하다. 게다가 맛있다."/>
+
+
+                                <img src='/img/info/about/artist/09_jangsoonkyu.jpg' title="장순규<br/>다양성이 존재해서 즐겁고 재미있는 아이디어를 만들게 해주는 곳!"/>
+                                <img src='/img/info/about/artist/08_jangsukwoo.jpg' title="장석우(1000day)<br/>노트폴리오는 젊음과 열정의 장소다."/>
+                                <img src='/img/info/about/artist/07_leegwangjoon.jpg' title="이광준<br/>사회를 만들기 전에 사람들은 모여 한 문화를 이루어 냈다. 문화를 만들고 문화를 즐기다 보면 그 앞에 노트폴리오가 있다. 노트폴리오는 새로운 문화 아이콘이다."/>
+                                <img src='/img/info/about/artist/06_umjihyun.jpg' title="엄지현<br/>나는 올해 디자이너로 나의 길을 완전히 확정지었다. 그와 동시에 정말 우연히 노트폴리오를 알게 되었고, 그 우연 덕분에 너무도 좋은 분들과 인연이 닿았다. 노트폴리오는 내게 신이 주신 새로운 인연의 장이다."/>
+
+                                <img src='/img/info/about/artist/05_byunsangwon.jpg' title="변상원<br/>다양한 사람들의 생각과 철학을 공유해주며 내 생각의 자극을 유도하는 촉매제"/>
+
+                                <img src='/img/info/about/artist/14_hanjaeyul.jpg' title="한재열<br/>세상에 이렇게나 많은 아티스트가 있다는 것, 여러분이 노트폴리오를 알았다는 것."/>
+                                <img src='/img/info/about/artist/13_choiyonglae.jpg 'title="최용래(비오코)<br/>New idea(새로운 아이디어)와<br/>Tv(재미와 감동)이 있고<br/>Forum(열정을 토론)하며<br/>Lotto(기다림과 설렘)이 느껴지는<br/>Open mind(열린 생각들)이 보이는 곳"/>
+                                <img src='/img/info/about/artist/12_joojaebum.jpg' title="주재범<br/>노트폴리오는 만화책 같아요. 딱딱한 구성방식으로 딱딱한 정보가 담긴 책보단 내게 맞는 좋은 정보를 만화처럼 구성한, 이미지 그리듯 잘 와닿게 표현한 즐거운 책. 가볍고 편안함이 있는 그런 책."/>
+                                <img src='/img/info/about/artist/11_jokisuk.jpg' title="조기석<br/>중매자. 다양한 사람들이 서로의 작업들을 교류하고 서로가 연결되는 곳. 내가 노트폴리오를 통해 새로운 작업을 할 수 있었던 것처럼."/>
+                                <img src='/img/info/about/artist/10_jangjaemin.jpg' title="장재민<br/>노트폴리오는 신진디자이너들의 플랫폼이 되기 위해 스스로 먼저 그들의 팬이 되어주는 바람직한 매체다. 작가에게 먼저 추파를 던지는 곳, 늘 진심을 다해 그의 팬 1호가 되어주는 곳."/>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -42,7 +81,21 @@
                     노트폴리오는 누구에게나 열려 있습니다. 함께 할 수 있는 일이 있다면 언제든 얘기해주세요 :)
                 </p>
                 <div class="well" style="height: 400px;">
-                    함께하는 사람들
+                    <a target="_blank" href="http://www.studiogoindol.com"><img src='/img/info/about/cooperation/01_goindol.png'/></a>
+                    <img src='/img/info/about/cooperation/02_KMU.png'/>
+                    <a target="_blank" href="http://www.facebook.com/hardworkLAB?fref=ts"><img src='/img/info/about/cooperation/03_nodong.png'/></a>
+                    <a target="_blank" href="http://dahalab.com/?page_id=19"><img src='/img/info/about/cooperation/04_daha.png'/></a>
+                    <a target="_blank" href="http://www.facebook.com/hellodamdam?fref=ts"><img src='/img/info/about/cooperation/05_damdam.png'/></a>
+                    <a target="_blank" href=><img src='/img/info/about/cooperation/06_dim.png'/></a>
+                    <a target="_blank" href="http://www.roym.kr"><img src='/img/info/about/cooperation/07_roym.png'/></a>
+                    <a target="_blank" href="http://www.facebook.com/pages/%EB%AD%90%ED%95%B4_%EA%B5%AD%EB%AF%BC%EB%8C%80-%EC%A1%B0%ED%98%95%EB%8C%80-%EC%9E%91%EC%97%85-%EA%B5%90%EB%A5%98%EC%A7%80/213986465331477?fref=ts"><img src='/img/info/about/cooperation/08_mohae.png'/></a>
+                    <a target="_blank" href="http://www.square-triangle.co.kr"><img src='/img/info/about/cooperation/09_squate.png'/></a>
+                    <a target="_blank" href="http://www.facebook.com/profile.php?id=100003119226316&fref=ts"><img src='/img/info/about/cooperation/10_samilo.png'/></a>
+                    <a target="_blank" href="http://www.actionstart.org"><img src='/img/info/about/cooperation/11_actionstart.png'/></a>
+                    <img src='/img/info/about/cooperation/12_yeonhapjeonsi.png'/>
+                    <a target="_blank" href="http://cafe.naver.com/characteri"><img src='/img/info/about/cooperation/13_characteri.png'/></a>
+                    <a target="_blank" href="http://www.cultureplatform.com"><img src='/img/info/about/cooperation/14_cultureplatform.png'/></a>
+                    <a target="_blank" href="http://www.hydraft.co.kr"><img src='/img/info/about/cooperation/15_hydraft.png'/></a>
                 </div>
 
             </div>
