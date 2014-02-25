0a93a99b21229bec92952283d606698654d48575
Yoon, Seongsu <sople1@snooey.net>
2014-02-25 20:26:50 +0900

    1

--- app/config/config.php
+++ app/config/config.php
@@ -270,7 +270,7 @@ $config['sess_time_to_update']	= 24*60*60;
 |
 */
 $config['cookie_prefix']	= "";
-$config['cookie_domain']	= "";
+$config['cookie_domain']    = '.'.(str_replace("www.", "", $_SERVER['HTTP_HOST']));
 $config['cookie_path']		= "/";
 $config['cookie_secure']	= FALSE;
 
