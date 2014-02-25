50b54fd8dbe773cc8f9f9a8f05be6430e980cc0a
Yoon, Seongsu <sople1@snooey.net>
2014-02-25 17:12:31 +0900

    1

--- app/libraries/file_save.php
+++ app/libraries/file_save.php
@@ -218,6 +218,11 @@ class file_save {
                 $image->setResolution(300,300); 
                 $image->readImage($tmp_name);
 
+                var_export(strlen($image->getImagesBlob())); // int(4316519)
+                echo("|");
+                var_export(strlen($image->getImageBlob()));  // int(61413)
+                echo("|");
+
                 $format = $image->getImageFormat();
                 if ($format == 'GIF') {
                     $image = $image->coalesceImages();
@@ -239,6 +244,11 @@ class file_save {
                     $image->profileImage('icc', $icc_rgb); 
                     unset($icc_rgb); 
                 }
+
+                var_export(strlen($image->getImagesBlob())); // int(4316519)
+                echo("|");
+                var_export(strlen($image->getImageBlob()));  // int(61413)
+                echo("|");
 
                 if(in_array('crop', $todo)){
                     // Crop Image. Resize is next block.
@@ -264,6 +274,11 @@ class file_save {
 
                 }
 
+                var_export(strlen($image->getImagesBlob())); // int(4316519)
+                echo("|");
+                var_export(strlen($image->getImageBlob()));  // int(61413)
+                echo("|");
+
                 //$image->resampleImage(200,200,imagick::FILTER_LANCZOS,1);
 
                 if(in_array('resize', $todo)){
@@ -282,6 +297,11 @@ class file_save {
                     }
                 }
 
+                var_export(strlen($image->getImagesBlob())); // int(4316519)
+                echo("|");
+                var_export(strlen($image->getImageBlob()));  // int(61413)
+                echo("|");
+
                 if ($format == 'GIF') {
                     $image->setImageBackgroundColor('none'); //This is important!
                     $image = $image->deconstructImages();
@@ -299,6 +319,13 @@ class file_save {
                     // Clean
                     $image->stripImage();
                 }
+
+                var_export(strlen($image->getImagesBlob())); // int(4316519)
+                echo("|");
+                var_export(strlen($image->getImageBlob()));  // int(61413)
+                echo("|");
+
+                exit();
 
                 // Save
                 $image->writeImages((($format=='GIF')?'gif:':'jpg:').$name, true);
