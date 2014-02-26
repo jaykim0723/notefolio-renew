74b5e0e76a6705413577c0e2e59f01328c35f867
Yoon, Seongsu <sople1@snooey.net>
2014-02-26 18:48:56 +0900

    1

--- app/views/profile/thumbnail_inc_view.php
+++ app/views/profile/thumbnail_inc_view.php
@@ -1,6 +1,6 @@
 <li class="thumbbox infinite-item <?php echo $row->status ?>">
 	<a href="/<?php echo $row->user->username ?>/<?php echo $row->work_id ?>">
-		<img src="/data/covers/<?=($row->work_id)?>_t1.jpg"/><br>
-		<p><?php echo trim(htmlentities($row->title, ENT_COMPAT, 'utf-8')); ?></p>
+		<img src="/data/covers/<?=($row->work_id)?>_t1.jpg"/>
+		<p style="text-align:left;"><?php echo trim(htmlentities($row->title, ENT_COMPAT, 'utf-8')); ?></p>
 	</a>
 </li>
