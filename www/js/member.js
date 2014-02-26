874772722a09cb4e9f221c06c756d00a080dc6bb
Yoon, Seongsu <sople1@snooey.net>
2014-02-27 01:32:52 +0900

    1

--- www/js/member.js
+++ www/js/member.js
@@ -616,11 +616,11 @@ var workUtil = {
 
 				case 'text':
 				default:
-					//var textarea = $('<textarea placeholder="이곳을 눌러 내용을 입력하세요"></textarea>').val(nl2br(c));
-					//$(target).append(textarea).find('textarea').cleditor(workUtil.clConf);
-					if(c=='') c = "이곳을 눌러 내용을 입력하세요";
-					var content = $('<div class="text-content"></div>').html(nl2br(c)).notebook();
-					$(target).append(content);
+					var textarea = $('<textarea placeholder="이곳을 눌러 내용을 입력하세요"></textarea>').val(nl2br(c));
+					$(target).append(textarea).find('textarea').cleditor(workUtil.clConf);
+					//if(c=='') c = "이곳을 눌러 내용을 입력하세요";
+					//var content = $('<div class="text-content"></div>').html(nl2br(c)).notebook();
+					//$(target).append(content);
 				break;
 			}
 			return true;
