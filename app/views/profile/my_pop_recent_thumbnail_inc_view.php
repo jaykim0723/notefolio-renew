ba2989c4030304f8c3abc2310519deb18fd6cc69
Yoon, Seongsu <sople1@snooey.net>
2014-02-26 11:27:53 +0900

    1

--- app/views/profile/my_pop_recent_thumbnail_inc_view.php
+++ app/views/profile/my_pop_recent_thumbnail_inc_view.php
@@ -1,4 +1,4 @@
-<li id="work-recent-<?php echo $row->work_id ?>" class="<?php echo $row->regdate < $this->config->item('disabled_cover') ? 'disabled' : '' ?>">
+<li id="work-recent-<?php echo $row->work_id ?>" class="<?php echo $row->moddate < $this->config->item('disabled_cover') ? 'disabled' : '' ?>">
 	<img src="/data/covers/<?php echo $row->work_id?>_t2.jpg?_=<?php echo substr($row->moddate, -2) ?>"/>
 	<?php echo htmlentities($row->title, ENT_COMPAT, 'utf-8'); ?>
 </li>
