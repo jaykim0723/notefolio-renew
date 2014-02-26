1754a86e6029e6bcb7b4a2703e0c061a732c177f
Yoon, Seongsu <sople1@snooey.net>
2014-02-27 01:39:46 +0900

    1

--- www/js/member.js
+++ www/js/member.js
@@ -624,7 +624,7 @@ var workUtil = {
 				default:
 					//var textarea = $('<textarea placeholder="이곳을 눌러 내용을 입력하세요"></textarea>').val(nl2br(c));
 					//$(target).append(textarea).find('textarea').cleditor(workUtil.clConf);
-					var content = $('<div class="text-content"></div>').html(nl2br(c)).notebook(workUtil.notebookConf);
+					var content = $('<div class="text-content"></div>').html(nl2br(c));
 					$(target).append(content);
 				break;
 			}
