9608bc53cb72a6c60303dee4b53cf2200e727a3a
Yoon, Seongsu <sople1@snooey.net>
2014-02-27 01:04:42 +0900

    Revert 6fa2ca6..3127477
    This rolls back to commit 6fa2ca6baf7d096783f7cd4e6160833ceed31e03.

--- app/views/layout/header_inc_view.php
+++ app/views/layout/header_inc_view.php
@@ -35,9 +35,6 @@
 
 	<link href='http://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,400,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'/>
 	<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'/>
-
-	<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="/css/jquery.notebook.css">
 	
 	<link href="https://s3.amazonaws.com/css_sprites/543/10543/ebc4aa8095.css" rel="stylesheet"/>
 	<link href="https://s3.amazonaws.com/css_sprites/713/10713/768bef68df.css" rel="stylesheet"/>
