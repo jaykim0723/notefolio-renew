04ebe0cc0882f780b3fe635d95d82e31c8bfd0f4
Yoon, Seongsu <sople1@snooey.net>
2014-02-28 18:20:55 +0900

    1

--- app/views/gallery/info_view.php
+++ app/views/gallery/info_view.php
@@ -262,11 +262,6 @@
 
 				<?php if ($row->prev_work_id!=0): ?>
 					<a href="/<?php echo $row->user->username; ?>/<?php echo $row->prev_work_id; ?>" class="more-link btn btn-default btn-block btn-more">more</a>
-				<?php elseif ($row->next_work_id!=0): ?>
-					<script>
-						$('#btn-prev-work').trigger('click');
-					</script>
-					<a href="/<?php echo $row->user->username; ?>/<?php echo $row->next_work_id; ?>" class="more-link btn btn-default btn-block btn-more">more</a>
 				<?php endif ?>
 				
 
