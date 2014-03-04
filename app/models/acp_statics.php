69faab0f8191c3bd6208699f5022b7f105696ab5
Yoon, Seongsu <sople1@snooey.net>
2014-03-04 14:25:04 +0900

    1

--- app/models/acp_statics.php
+++ app/models/acp_statics.php
@@ -1017,7 +1017,15 @@ class Acp_statics extends CI_Model
         $this->load->config('keyword', TRUE);
         $keyword_list = $this->config->item('keyword', 'keyword');
         foreach ($keyword_list as $key => $keyword) { 
-            $sql .= "(SELECT '".$keyword."' as category, count(work_id) as count, 0 as all_count from works where keyword like '%".$key."%') UNION ALL ";
+            $sql .= "(SELECT '".$keyword."' as category, count(work_id) as count, 0 as all_count from works where keywords like '%".$key."%')";
+
+            if($i == count($keyword_list)){
+                $i = 1;
+            }
+            else {
+                $i++;
+                $sql .= " UNION ALL ";
+            }
         }
         $sql .= ") categories limit 0, ?";
 //join (select count(id) as all_count from work_categories) allCount group by category order by count desc 
