aa39381d6d6c7cb7608d64a0c0905fac6566a6ff
Yoon, Seongsu <sople1@snooey.net>
2014-03-04 14:09:24 +0900

    1

--- app/models/acp_statics.php
+++ app/models/acp_statics.php
@@ -927,7 +927,7 @@ class Acp_statics extends CI_Model
         $sql = "SELECT date, note_count, work_count 
                     FROM (SELECT DATE_FORMAT(regdate, '%Y년 %m월') as date, count(id) as note_count 
                         FROM log_work_note WHERE regdate >= ? and regdate <= ? group by date) notes 
-                    left join (SELECT DATE_FORMAT(regdate, '%Y년 %m월') as work_date, count(id) as work_count 
+                    left join (SELECT DATE_FORMAT(regdate, '%Y년 %m월') as work_date, count(work_id) as work_count 
                         FROM works WHERE regdate >= ? and regdate <= ? group by work_date) works on notes.date=works.work_date;"; 
         $query = $this->db->query($sql, array($from, $to, $from, $to));
         
