dd363f23ce4747cafbba1401d634b9b4ef7240d5
Yoon, Seongsu <sople1@snooey.net>
2014-03-05 18:13:41 +0900

    1

--- app/models/work_model.php
+++ app/models/work_model.php
@@ -65,7 +65,7 @@ class work_model extends CI_Model {
                 break;
         }
         if($params->from!='all'){
-            $this->db->having("(works.regdate >= ".$this->db->escape($from).")", NULL, FALSE); // 모든 기준이 regdate로 하기 때문에
+            $this->db->where("(works.regdate >= ".$this->db->escape($from).")", NULL, FALSE); // 모든 기준이 regdate로 하기 때문에
             // $this->db->having("(works.regdate >= ".$this->db->escape($from)." or works.moddate >= ".$this->db->escape($from).")", NULL, FALSE);
         }
 
