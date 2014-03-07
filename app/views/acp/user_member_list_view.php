7658c926819a302c4368fc7f22698f25cc07d6c5
Yoon, Seongsu <sople1@snooey.net>
2014-03-07 13:21:10 +0900

    1

--- app/views/acp/user_member_list_view.php
+++ app/views/acp/user_member_list_view.php
@@ -41,10 +41,6 @@
                   <option value="idsmaller"<?=($args['order']=="idsmaller")?' selected':''?>>번호순</option>
                   <option value="newest"<?=($args['order']=="newest")?' selected':''?>>최신순</option>
                   <option value="oldest"<?=($args['order']=="oldest")?' selected':''?>>과거순</option>
-                  <option value="noted"<?=($args['order']=="noted")?' selected':''?>>인기순</option>
-                  <option value="viewed"<?=($args['order']=="viewed")?' selected':''?>>조회순</option>
-                  <option value="nofol_rank"<?=($args['order']=="nofol_rank")?' selected':''?>>노폴랭크순</option>
-                  <option value="comment_desc"<?=($args['order']=="comment_desc")?' selected':''?>>댓글순</option>
                 </select>
               </div>
               
