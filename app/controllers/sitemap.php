9fa4098b9e02d3940a09f1cb8348e7ce0fe6e19b
Yoon, Seongsu <sople1@snooey.net>
2014-03-14 17:05:53 +0900

    1

--- app/controllers/sitemap.php
+++ app/controllers/sitemap.php
@@ -161,43 +161,12 @@ class Sitemap extends CI_Controller {
 			return $output;
 		}
 
-		function make_resource_works($username, $total){
-			$output = array();
-
-			$CI =& get_instance();
-
-	        $CI->load->model('work_model');
-
-			$work_list = $CI->work_model->get_list(array(
-				'page' => 1,
-				'delimiter' => $total,
-				'user_id' => $CI->user_id
-			));
-
-			$output[] = (object)array(
-				'loc'=>$username.'/'.$work->row->,
-				'lastmod'=>time(),
-				'changefreq'=>'always',
-				'priority'=>1.0
-				);
-
-			return $output;
-		}
-
 		if(empty($username)){
 
 		} else {
 			$resource = array_merge(
 				(array)$resource, 
 				(array)make_resource_profile($username)
-				);
-
-			$user_id = $this->profile_model->get_user_id_from_username($username);
-			$total = $this->profile_model->get_statistics_total(array('user_id'=>$user_id))->row;
-
-			$resource = array_merge(
-				(array)$resource, 
-				(array)make_resource_works($username)
 				);
 		}
 
