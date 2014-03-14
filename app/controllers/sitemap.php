de852aa94586d55167e804f19ab22d6398a367ac
Yoon, Seongsu <sople1@snooey.net>
2014-03-14 14:38:32 +0900

    1

--- app/controllers/sitemap.php
+++ app/controllers/sitemap.php
@@ -5,6 +5,7 @@ class Sitemap extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
+        $this->output->set_content_type('text/xml');
     }
 	
     /**
