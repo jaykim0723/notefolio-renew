049122e7704b4af5cf96d5ab17a47d9cec9d17b8
Yoon, Seongsu <sople1@snooey.net>
2014-03-14 14:29:48 +0900

    1

--- app/controllers/sitemap.php
+++ app/controllers/sitemap.php
@@ -21,47 +21,6 @@ class Sitemap extends CI_Controller {
 			);
 		$this->load->view('sitemap/index_view', $data);
 	}
-	
-    /**
-     * urlset of sitemap
-	 *
-	 */
-	public function root()
-	{	
-		$resource = array(
-			(object)array(
-				'loc'=>'/',
-				'lastmod'=>time(),
-				'changefreq'=>'always'
-				'priority'=>0.7
-				),
-			(object)array(
-				'loc'=>'/auth/login',
-				'lastmod'=>time(),
-				'changefreq'=>'monthly'
-				'priority'=>0.3
-				),
-			(object)array(
-				'loc'=>'/auth/setting',
-				'lastmod'=>time(),
-				'changefreq'=>'monthly'
-				'priority'=>0.3
-				),
-			);
-
-		$data = array();
-
-		foreach($resource as $key=>$val){
-			$data[] = (object)array(
-				'loc'			=> $val->loc,
-        		'lastmod'		=> date('c',$val->lastmod),
-        		'changefreq'	=> $val->changefreq,
-        		'priority'		=> $val->priorty,
-				);
-		}
-
-		$this->load->view('sitemap/urlset_view', $data);
-	}
 }
 
 /* End of file sitemap.php */
