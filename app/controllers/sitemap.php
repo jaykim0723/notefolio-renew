f188c5cdf0444d237feebc3471b519e501d79a92
Yoon, Seongsu <sople1@snooey.net>
2014-03-14 13:35:03 +0900

    1

--- app/controllers/sitemap.php
+++ app/controllers/sitemap.php
@@ -1,41 +1,11 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-exit('a');
+
 class Sitemap extends CI_Controller {
 
     public function __construct()
     {
         parent::__construct();
     }
-	
-    /**
-     * index of sitemap
-	 *
-	 */
-	public function index()
-	{	
-		$data = array(
-			'list'=> array(
-				'default' => date('c',time()),
-				'user' => date('c',time()),
-				),
-			);
-		$this->load->view('sitemap/index_view', $data);
-	}
-	
-    /**
-     * urlset of sitemap
-	 *
-	 */
-	public function default()
-	{	
-		$data = array(
-			'list'=> array(
-				'default' => date('c',time()),
-				'user' => date('c',time()),
-				),
-			);
-		$this->load->view('sitemap/urlset_view', $data);
-	}
 }
 
 /* End of file sitemap.php */
